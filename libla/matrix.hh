2d7be874e079fd7f6fa096a46141d2c3afe4298e
Danny van Dyk <dyk@honei.org>
2007-05-14 16:03:29 +0000

    Split out DenseMatrix to its own header file. Added interface for mutable matrices.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@36 0f804a50-a62f-0410-84e1-872c020a11f9

--- libla/matrix.hh
+++ libla/matrix.hh
@@ -22,12 +22,11 @@
 #ifndef LIBLA_GUARD_MATRIX_HH
 #define LIBLA_GUARD_MATRIX_HH 1
 
-#include <iostream>
-
 #include <libla/element_iterator.hh>
 #include <libla/vector.hh>
 #include <libutil/shared_array.hh>
 
+#include <ostream>
 #include <string.h>
 #include <iterator>
 
@@ -71,7 +70,15 @@ namespace pg512 ///< \todo Namespace name?
 
             /// Retrieves element by index, zero-based, unassignable.
             virtual const Vector<DataType_> & operator[] (unsigned long row) const = 0;
+    };
 
+    /**
+     * A MutableMatrix is the abstract baseclass for all mutable matrix-like types used.
+     **/
+    template <typename DataType_> class MutableMatrix :
+        public Matrix<DataType_>
+    {
+        public:
             /// Retrieves element by index, zero-based, assignable
             virtual Vector<DataType_> & operator[] (unsigned long row) = 0;
     };
@@ -95,186 +102,6 @@ namespace pg512 ///< \todo Namespace name?
 
         return lhs;
     }
-
-    /**
-     * A DenseMatrix is a matrix with O(column * row) non-zero elements which keeps its data
-     * sequential.
-     **/
-    template <typename DataType_> class DenseMatrix :
-        public Matrix<DataType_>
-    {
-        private:
-            /// Pointer to our elements.
-            SharedArray<DataType_> _elements;
-
-            /// Our columns.
-            unsigned long _columns;
-
-            /// Our rows.
-            unsigned long _rows;
-
-            /// Our row-vectors.
-            SharedArray<std::tr1::shared_ptr<DenseVector<DataType_> > > _row_vectors;
-
-        public:
-            /// Our implementation of ElementIterator.
-            template <typename ElementType_> class ElementIteratorImpl;
-            friend class ElementIteratorImpl<DataType_>;
-
-            /// Type of the iterator over our elements.
-            typedef ElementIteratorWrapper<Matrix<DataType_>, DataType_> ElementIterator;
-
-            /// Type of the iterator over our row/column/band/diagonal vectors.
-//            typedef VectorIteratorBase<Matrix<DataType_>, Vector<DataType_> > VectorIterator;
-
-            /// Constructor.
-            DenseMatrix(unsigned long columns, unsigned long rows) :
-                _elements(new DataType_[rows * columns]),
-                _columns(columns),
-                _rows(rows),
-                _row_vectors(new std::tr1::shared_ptr<DenseVector<DataType_> >[rows])
-            {
-            }
-
-            /// Constructor.
-            DenseMatrix(unsigned long columns, unsigned long rows, DataType_ value) :
-                _elements(new DataType_[rows * columns]),
-                _columns(columns),
-                _rows(rows),
-                _row_vectors(new std::tr1::shared_ptr<DenseVector<DataType_> >[rows])
-            {
-                for (unsigned long i(0) ; i < rows * columns ; ++i)
-                    _elements[i] = value;
-            }
-
-            /// Returns iterator pointing to the first element of the matrix.
-            virtual ElementIterator begin_elements() const
-            {
-                return ElementIterator(new ElementIteratorImpl<DataType_>(*this, 0));
-            }
-
-            /// Returns iterator pointing behind the last element of the vector.
-            virtual ElementIterator end_elements() const
-            {
-                return ElementIterator(new ElementIteratorImpl<DataType_>(*this, _rows * _columns));
-            }
-
-            /// Returns our columns.
-            virtual unsigned long columns() const
-            {
-                return _columns;
-            }
-
-            /// Returns our rows.
-            virtual unsigned long rows() const
-            {
-                return _rows;
-            }
-
-            /// Retrieves element by index, zero-based, assignable.
-            virtual const Vector<DataType_> & operator[] (unsigned long row) const
-            {
-                if (! _row_vectors[row])
-                    _row_vectors[row].reset(new DenseVector<DataType_>(_columns, _elements, row * _columns, 1));
-
-                return *_row_vectors[row];
-            }
-
-            /// Retrieves element by index, zero-based, assignable.
-            virtual Vector<DataType_> & operator[] (unsigned long row)
-            {
-                if (! _row_vectors[row])
-                    _row_vectors[row].reset(new DenseVector<DataType_>(_columns, _elements, row * _columns, 1));
-
-                return *_row_vectors[row];
-            }
-    };
-
-    /**
-     * A DenseMatrix::ElementIteratorImpl is a simple iterator implementation for dense matrices.
-     **/
-    template <> template <typename DataType_> class DenseMatrix<DataType_>::ElementIteratorImpl<DataType_> :
-        public ElementIteratorImplBase<Matrix<DataType_>, DataType_>
-    {
-        private:
-            /// Our matrix.
-            const DenseMatrix<DataType_> & _matrix;
-
-            /// Our index.
-            unsigned long _index;
-
-        public:
-            /// Constructor.
-            ElementIteratorImpl(const DenseMatrix<DataType_> & matrix, unsigned long index) :
-                _matrix(matrix),
-                _index(index)
-            {
-            }
-
-            /// Constructor.
-            ElementIteratorImpl(ElementIteratorImpl<DataType_> const & other) :
-                _matrix(other._matrix),
-                _index(other._index)
-            {
-            }
-
-            /// Preincrement operator.
-            virtual ElementIteratorImpl<DataType_> & operator++ ()
-            {
-                ++_index;
-                return *this;
-            }
-
-            /// Postincrement operator.
-            virtual ElementIteratorImpl<DataType_> operator++ (int)
-            {
-                ElementIteratorImpl<DataType_> result(*this);
-                ++_index;
-                return result;
-            }
-
-            /// Equality operator.
-            virtual bool operator== (const ElementIteratorImplBase<Matrix<DataType_>, DataType_> & other) const
-            {
-                return (&_matrix == other.parent()) && (_index == other.index());
-            }
-
-            /// Inequality operator.
-            virtual bool operator!= (const ElementIteratorImplBase<Matrix<DataType_>, DataType_> & other) const
-            {
-                return ((&_matrix != other.parent()) || (_index != other.index()));
-            }
-
-            /// Dereference operator 
-            virtual DataType_ & operator* () const
-            {
-                return _matrix._elements[_index];
-            }
-
-            /// Our index.
-            virtual const unsigned long index() const
-            {
-                return _index;
-            }
-
-            /// Our column.
-            virtual const unsigned long column() const
-            {
-                return _index % _matrix._rows;
-            }
-
-            /// Our row.
-            virtual const unsigned long row() const
-            {
-                return _index / _matrix._rows;
-            }
-
-            /// Our parent.
-            virtual const Matrix<DataType_> * parent() const
-            {
-                return &_matrix;
-            }
-    };
 }
 
 #endif
