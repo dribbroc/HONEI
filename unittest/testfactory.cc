c014b69fcecbceb5fd916e29f6877d3da24232c5
Dirk Ribbrock <ribbrock@honei.org>
2007-05-15 08:50:26 +0000

    unittest/testfactory.cc unittest/testfactory.hh: Added rudimental classfactory
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@37 0f804a50-a62f-0410-84e1-872c020a11f9

---
+++ unittest/testfactory.cc
@@ -0,0 +1,13 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+#include "testfactory.hh"
+
+
+BaseTest * TestFactory::NewTest (const string & description)
+{/*
+    if (description == "BlasTest")
+        return new BlasTest;
+    if (description == "LiblaTest")
+        return new LiblaTest;
+*/    return NULL;
+}
+
---
+++ unittest/testfactory.hh
@@ -0,0 +1,15 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+#ifndef TESTFACTORY_HH
+#define TESTFACTORYT_HH 1
+#include <string>
+#include "basetest.hh"
+#include "blastest.hh"
+#include "liblatest.hh"
+using namespace std;	
+class TestFactory
+{
+	public:
+	static BaseTest * NewTest (const string & description);
+};
+
+#endif
