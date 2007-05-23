30677dfd8c3a8be7d134e4717fb2f9825203ae07
Danny van Dyk <dyk@honei.org>
2007-05-23 12:26:40 +0000

    Added unittest directory. Applied coding conventions. Sanitized TEST_CHECK_EQUAL_WITHIN_EPS. Fixed includes and namespaces.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@61 0f804a50-a62f-0410-84e1-872c020a11f9

--- unittest/unittest.cc
+++ unittest/unittest.cc
@@ -1,102 +1,92 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
-#ifdef MOO
+
 #include <unittest/unittest.hh>
-using namespace std;
+
+#include <libwrapiter/libwrapiter_forward_iterator.hh>
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <list>
+#include <string>
+#include <utility>
 
 class BaseTest;
 
 class TestList
 {
     private:
-        static list<BasteTest *> _tests;
-        
+        static std::list<BaseTest *> _tests;
+
         TestList()
         {
         }
-        
+
     public:
-        typedef libwrapiter::ForwardIterator<TestList, list<BaseTest*>::value_type> Iterator;
+        typedef libwrapiter::ForwardIterator<TestList, std::list<BaseTest*>::value_type> Iterator;
 
         static TestList * instance()
         {
             static TestList result;
+
             return &result;
         }
-        
+
         void register_test(BaseTest * const test)
         {
             _tests.push_back(test);
         }
-        
+
         Iterator begin_tests() const
         {
             return Iterator(_tests.begin());
         }
-        
+
         Iterator end_tests() const
         {
             return Iterator(_tests.end());
         }
 };
 
-list<BaseTest *> TestList::_tests;
+std::list<BaseTest *> TestList::_tests;
 
-        BaseTest::BaseTest(const string & id) : _id(id)
-        {
-            TestList::instance()->register_test(this);
-        }
-        
-        const string BaseTest::id() const
-        {
-            return _id;
-        }
+BaseTest::BaseTest(const std::string & id) :
+    _id(id)
+{
+    TestList::instance()->register_test(this);
+}
 
-        void BaseTest::check(const char * const function, const char * const file,
- 	        const long line, bool was_ok, const std::string & message) const
-	 	{
-	 	    std::cout << "." << std::flush;
-	 	    if (! was_ok)
-	 	        throw TestFailedException(function, file, line, message);
-	 	}
+const std::string
+BaseTest::id() const
+{
+    return _id;
+}
 
-
-
-
+void
+BaseTest::check(const char * const function, const char * const file,
+    const long line, bool was_ok, const std::string & message) const
+{
+    std::cout << "." << std::flush;
+    if (! was_ok)
+        throw TestFailedException(function, file, line, message);
+}
 
 int main(int argc, char** argv) 
 {
+    int result(EXIT_FAILURE);
 
-    /*cout << "Hallo du!\n";
-    cout << argc << " Argumente. Wert:  " << argv[1];
-    cout << "\n\n";
-
-    cout << "Wieviele Parameter ausgeben?" << endl;
-
-    int anzahl;
-    cin >> anzahl;
-    cout << anzahl << endl;
-    	
-    for (int i = 0; (i < anzahl) && (i < argc); i++) {
-    	cout << argv[i] << endl; 
-    } */
-    
     for (TestList::Iterator i(TestList::instance()->begin_tests()),i_end(TestList::instance()->end_tests()) ; i != i_end ; ++i)
     {
         try
         {
             (*i)->run();
+            std::cout << (*i)->id() << ": PASSED" << std::endl;
+            result = EXIT_SUCCESS;
         }
-        catch (TestError & e)
+        catch (TestFailedException & e)
         {
-            
+            std::cout << (*i)->id() << ": FAILED" << std::endl;
         }
     }
-	return 0;
+
+    return result;
 }
-#endif
-
-
-
-
-
- 
--- unittest/unittest.hh
+++ unittest/unittest.hh
@@ -1,208 +1,205 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
-#ifdef MOO
-#ifndef UNITTEST_HH
-#define UNITTEST_HH
+#ifndef UNITTEST_GUARD_UNITTEST_HH
+#define UNITTEST_GUARD_UNITTEST_HH 1
 
-#include <iostream>
-#include <string>
-#include <exception>
-#include <utility>
-#include <list>
-#include <libwraptiter/libwrapiter_forward_iterator.hh>
 #include <libutil/stringify.hh>
 
-using namespace std;
+#include <string>
+#include <exception>
 
 class BaseTest
 {
     protected:
-        const string _id;
+        const std::string _id;
+
     public:
-        BaseTest(const string & id) : _id(id);
-        
-        const string id() const;
-        
-        
+        BaseTest(const std::string & id);
+
+        const std::string id() const;
+
+        void check(const char * const function, const char * const file,
+                const long line, bool was_ok, const std::string & message) const;
+
         virtual void run() const = 0;
         virtual void benchmark() const = 0;
 
-        void check(const char * const function, const char * const file,
- 	        const long line, bool was_ok, const std::string & message) const;	
+        /**
+         * Utility class used by TEST_CHECK_EQUAL.
+         */
+        struct TwoVarHolder
+        {
+            bool result;
+            std::string s_a;
+            std::string s_b;
 
-    /**
- 	     * Utility class used by TEST_CHECK_EQUAL.
- 	     */
- 	    struct TwoVarHolder
- 	    {
- 	        bool result;
- 	        std::string s_a;
- 	        std::string s_b;
- 	
- 	        template <typename T1_, typename T2_>
- 	        TwoVarHolder(T1_ a, T2_ b) :
- 	            result(a == b),
- 	            s_a(stringify(a)),
- 	            s_b(stringify(b))
- 	        {
- 	        }
- 	    }; 	
+            template <typename T1_, typename T2_>
+            TwoVarHolder(T1_ a, T2_ b) :
+                result(a == b),
+                s_a(stringify(a)),
+                s_b(stringify(b))
+            {
+            }
+        };
 
-        struct TestEps        
- 	    {
- 	        bool result;
- 	        string s_a;
- 	        string s_b;
- 	        string s_diff;
- 	
- 	        template <typename T1_, typename T2_, typename T3_>
- 	        TwoVarHolder(T1_ a, T2_ b, T3_ c) :
- 	            result((a - b) < c),
- 	            s_diff(stringify(a-b)),
- 	            s_a(stringify(a)),
- 	            s_b(stringify(b))
- 	        {
- 	        }
- 	    }; 	
+        /**
+         * Utility class used by TEST_CHECK_EQUAL_WITHIN_EPS.
+         */
+        struct WithinEpsCalculator
+        {
+            bool result;
+            std::string s_a;
+            std::string s_b;
+            std::string s_diff;
+
+            template <typename T1_, typename T2_, typename T3_>
+            WithinEpsCalculator(T1_ a, T2_ b, T3_ c) :
+                result((a - b) < c),
+                s_diff(stringify(a-b)),
+                s_a(stringify(a)),
+                s_b(stringify(b))
+            {
+            }
+        };
 };
 
-class TestFailedException : public exception
- 	    {
- 	        private:
- 	            const string _message;
- 	
- 	        public:
- 	            /**
- 	             * Constructor.
- 	             */
- 	            TestFailedException(const char * const function, const char * const file,
- 	                    const long line, const string & message) throw ();
- 	
- 	            /**
- 	             * Destructor.
- 	             */
- 	            virtual ~TestFailedException() throw ();
- 	
- 	            /**
- 	             * Description.
- 	             */
- 	            const char * what() const throw ()
- 	            {
- 	                return _message.c_str();
- 	            }
- 	    };
+class TestFailedException :
+    public std::exception
+{
+    private:
+        const std::string _message;
+
+    public:
+        /**
+         * Constructor.
+         */
+        TestFailedException(const char * const function, const char * const file,
+            const long line, const std::string & message) throw ();
+
+        /**
+         * Destructor.
+         */
+        virtual ~TestFailedException() throw ();
+
+        /**
+         * Description.
+         */
+        const char * what() const throw ()
+        {
+            return _message.c_str();
+        }
+};
 
 /**
- 	 * Check that a == b.
- 	 */
- 	#define TEST_CHECK_EQUAL(a, b) \
- 	    do { \
- 	        try { \
- 	            TwoVarHolder test_h(a, b); \
- 	            check(__PRETTY_FUNCTION__, __FILE__, __LINE__, test_h.result, \
- 	                    "Expected '" #a "' to equal '" + test_h.s_b + \
- 	                    "' but got '" + test_h.s_a + "'"); \
- 	        } catch (const TestFailedException &) { \
- 	            throw; \
- 	        } catch (const std::exception & test_e) { \
- 	            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
- 	                    "Test threw unexpected exception " + \
- 	                    " inside a TEST_CHECK_EQUAL block"); \
- 	        } catch (...) { \
- 	            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
- 	                    "Test threw unexpected unknown exception inside a TEST_CHECK_EQUAL block"); \
- 	        } \
- 	    } while (false)
- 	
- 	/**
- 	 * Check that stringify(a) == stringify(b).
- 	 */
- 	#define TEST_CHECK_STRINGIFY_EQUAL(a, b) \
- 	    do { \
-          try { \
- 	            std::string s_a(stringify(a)); \
- 	            std::string s_b(stringify(b)); \
- 	            check(__PRETTY_FUNCTION__, __FILE__, __LINE__, s_a == s_b, \
- 	                    "Expected '" #a "' to equal '" + s_b + \
- 	                    "' but got '" + s_a + "'"); \
- 	        } catch (const TestFailedException &) { \
- 	            throw; \
- 	        } catch (const exception & test_e) { \
- 	            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
- 	                    "Test threw unexpected exception " + \
- 	                    " inside a TEST_CHECK_STRINGIFY_EQUAL block"); \
- 	        } catch (...) { \
- 	            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
- 	                    "Test threw unexpected unknown exception inside a TEST_CHECK_STRINGIFY_EQUAL block"); \
- 	        } \
- 	    } while (false)
- 	
- 	/**
- 	 * Check that a is true.
- 	 */
- 	#define TEST_CHECK(a) \
- 	    do { \
- 	        try { \
- 	            check(__PRETTY_FUNCTION__, __FILE__, __LINE__, a, \
- 	                    "Check '" #a "' failed"); \
- 	        } catch (const TestFailedException &) { \
- 	            throw; \
- 	        } catch (const std::exception & test_e) { \
- 	            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
- 	                    "Test threw unexpected exception "  + \
- 	                    " inside a TEST_CHECK block"); \
- 	        } catch (...) { \
- 	            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
- 	                    "Test threw unexpected unknown exception inside a TEST_CHECK block"); \
- 	        } \
- 	    } while (false)
- 	
- 	/**
- 	 * Check that a throws an exception of type b.
- 	 */
- 	#define TEST_CHECK_THROWS(a, b) \
- 	    do { \
- 	        try { \
- 	            try { \
-	                a; \
- 	                check(__PRETTY_FUNCTION__, __FILE__, __LINE__, false, \
- 	                        "Expected exception of type '" #b "' not thrown"); \
- 	            } catch (b &) { \
- 	                TEST_CHECK(true); \
- 	            } \
- 	        } catch (const TestFailedException &) { \
- 	            throw; \
- 	        } catch (const std::exception & test_e) { \
- 	            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
- 	                    "Test threw unexpected exception "  + \
- 	                    " inside a TEST_CHECK_THROWS block"); \
- 	        } catch (...) { \
- 	            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
- 	                    "Test threw unexpected unknown exception inside a TEST_CHECK_THROWS block"); \
- 	        } \
- 	    } while (false)
+ * Check that a == b.
+ */
+#define TEST_CHECK_EQUAL(a, b) \
+    do { \
+        try { \
+            TwoVarHolder test_h(a, b); \
+            check(__PRETTY_FUNCTION__, __FILE__, __LINE__, test_h.result, \
+                    "Expected '" #a "' to equal '" + test_h.s_b + \
+                    "' but got '" + test_h.s_a + "'"); \
+        } catch (const TestFailedException &) { \
+            throw; \
+        } catch (const std::exception & test_e) { \
+            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
+                    "Test threw unexpected exception " + \
+                    " inside a TEST_CHECK_EQUAL block"); \
+        } catch (...) { \
+            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
+                    "Test threw unexpected unknown exception inside a TEST_CHECK_EQUAL block"); \
+        } \
+    } while (false)
 
 /**
- 	 * Check that a -b < epsilon.
- 	 */
- 	#define TEST_CHECK_EQUAL_EPS(a, b, eps) \
- 	    do { \
- 	        try { \
- 	            TestEps test_eps(a, b, eps); \
- 	            check(__PRETTY_FUNCTION__, __FILE__, __LINE__, test_eps.result, \
- 	                    "Expected '" #a "' - '" + #b + \
- 	                    "' < '" + eps + "'but was'"+ test_eps.s_diff); \
- 	        } catch (const TestFailedException &) { \
- 	            throw; \
- 	        } catch (const std::exception & test_e) { \
- 	            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
- 	                    "Test threw unexpected exception " + \
- 	                    " inside a TEST_CHECK_EQUAL block"); \
- 	        } catch (...) { \
- 	            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
- 	                    "Test threw unexpected unknown exception inside a TEST_CHECK_EQUAL block"); \
- 	        } \
- 	    } while (false)
+ * Check that stringify(a) == stringify(b).
+ */
+#define TEST_CHECK_STRINGIFY_EQUAL(a, b) \
+    do { \
+  try { \
+            std::string s_a(stringify(a)); \
+            std::string s_b(stringify(b)); \
+            check(__PRETTY_FUNCTION__, __FILE__, __LINE__, s_a == s_b, \
+                    "Expected '" #a "' to equal '" + s_b + \
+                    "' but got '" + s_a + "'"); \
+        } catch (const TestFailedException &) { \
+            throw; \
+        } catch (const exception & test_e) { \
+            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
+                    "Test threw unexpected exception " + \
+                    " inside a TEST_CHECK_STRINGIFY_EQUAL block"); \
+        } catch (...) { \
+            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
+                    "Test threw unexpected unknown exception inside a TEST_CHECK_STRINGIFY_EQUAL block"); \
+        } \
+    } while (false)
 
+/**
+ * Check that a is true.
+ */
+#define TEST_CHECK(a) \
+    do { \
+        try { \
+            check(__PRETTY_FUNCTION__, __FILE__, __LINE__, a, \
+                    "Check '" #a "' failed"); \
+        } catch (const TestFailedException &) { \
+            throw; \
+        } catch (const std::exception & test_e) { \
+            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
+                    "Test threw unexpected exception "  + \
+                    " inside a TEST_CHECK block"); \
+        } catch (...) { \
+            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
+                    "Test threw unexpected unknown exception inside a TEST_CHECK block"); \
+        } \
+    } while (false)
+
+/**
+ * Check that a throws an exception of type b.
+ */
+#define TEST_CHECK_THROWS(a, b) \
+    do { \
+        try { \
+            try { \
+                a; \
+                check(__PRETTY_FUNCTION__, __FILE__, __LINE__, false, \
+                        "Expected exception of type '" #b "' not thrown"); \
+            } catch (b &) { \
+                TEST_CHECK(true); \
+            } \
+        } catch (const TestFailedException &) { \
+            throw; \
+        } catch (const std::exception & test_e) { \
+            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
+                    "Test threw unexpected exception "  + \
+                    " inside a TEST_CHECK_THROWS block"); \
+        } catch (...) { \
+            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
+                    "Test threw unexpected unknown exception inside a TEST_CHECK_THROWS block"); \
+        } \
+    } while (false)
+
+/**
+ * Check that a - b < epsilon.
+ */
+#define TEST_CHECK_EQUAL_WITHIN_EPS(a, b, eps) \
+    do { \
+        try { \
+            WithinEpsCalculator calc(a, b, eps); \
+            check(__PRETTY_FUNCTION__, __FILE__, __LINE__, calc.result, \
+                    "Expected '" #a "' - '" #b \
+                    "' < '" + stringify(eps) + "' but was '" + calc.s_diff); \
+        } catch (const TestFailedException &) { \
+            throw; \
+        } catch (const std::exception & test_e) { \
+            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
+                    "Test threw unexpected exception " + \
+                    " inside a TEST_CHECK_EQUAL_WITHIN_EPS block"); \
+        } catch (...) { \
+            throw TestFailedException(__PRETTY_FUNCTION__, __FILE__, __LINE__, \
+                    "Test threw unexpected unknown exception inside a TEST_CHECK_EQUAL_WITHIN_EPS block"); \
+        } \
+    } while (false)
 
 #endif
