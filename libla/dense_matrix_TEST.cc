fae7feb111ef011d9749809c317de3e0084b6e76
Dirk Ribbrock <ribbrock@honei.org>
2007-06-06 12:09:03 +0000

    Added DenseMatrixEqualityTest and a rudimental version of DenseMatrixFunctionsTest.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@127 0f804a50-a62f-0410-84e1-872c020a11f9

--- libla/dense_matrix_TEST.cc
+++ libla/dense_matrix_TEST.cc
@@ -1,6 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 #include <libla/dense_matrix.hh>
+#include <libla/vector.hh>
 #include <unittest/unittest.hh>
 
 #include <string>
@@ -31,3 +32,67 @@ class DenseMatrixCreationTest :
 
 DenseMatrixCreationTest<float> dense_matrix_creation_test_float("float");
 DenseMatrixCreationTest<double> dense_matrix_creation_test_double("double");
+
+template <typename DataType_>
+class DenseMatrixEqualityTest :
+    public BaseTest
+{
+    public:
+        DenseMatrixEqualityTest(const std::string & type) :
+            BaseTest("dense_matrix_equality_test<" + type + ">")
+        {
+        }
+
+        virtual void run() const
+        {
+            for (unsigned long size(10) ; size < (1 << 10) ; size <<= 1)
+            {
+                std::tr1::shared_ptr<DenseMatrix<DataType_> > dm0(new DenseMatrix<DataType_>(size,
+                    size,static_cast<DataType_>(1)));
+                std::tr1::shared_ptr<DenseMatrix<DataType_> > dm1(new DenseMatrix<DataType_>(size,
+                    size,static_cast<DataType_>(1)));
+
+                TEST_CHECK(dm0==dm1);
+            }
+        }
+};
+DenseMatrixEqualityTest<bool> dense_matrix_equality_test_bool("bool");
+DenseMatrixEqualityTest<int> dense_matrix_equality_test_int("int");
+
+template <typename DataType_>
+class DenseMatrixFunctionsTest :
+    public BaseTest
+{
+    public:
+        DenseMatrixFunctionsTest(const std::string & type) :
+            BaseTest("dense_matrix_functions_test<" + type + ">")
+        {
+        }
+
+        virtual void run() const
+        {
+            for (unsigned long size(10) ; size < (1 << 10) ; size <<= 1)
+            {
+                std::tr1::shared_ptr<DenseMatrix<DataType_> > dm(new DenseMatrix<DataType_>(size,size+1));
+                for (typename MutableMatrix<DataType_>::ElementIterator i(dm->begin_elements()), 
+                    i_end(dm->end_elements()) ; i != i_end ; ++i)
+                {
+                    *i = static_cast<DataType_>(i.index());
+                }
+                TEST_CHECK_EQUAL(dm->columns(),size);
+                TEST_CHECK_EQUAL(dm->rows(),size+1);
+
+                Vector<DataType_> * row_vector = &((*dm)[0]);
+                Vector<DataType_> * column_vector = &(dm->column(0));                
+                TEST_CHECK_EQUAL(row_vector->size(),size+1);
+                TEST_CHECK_EQUAL(column_vector->size(),size);
+                
+                ///< \todo Fix compile errors in vector.hh and expand check over all elements of dm
+                //TEST_CHECK_EQUAL_WITHIN_EPS(row_vector[1],1,std::numeric_limits<DataType_>::epsilon());
+
+            }
+        }
+};
+
+DenseMatrixFunctionsTest<float> dense_matrix_functions_test_float("float");
+DenseMatrixFunctionsTest<double> dense_matrix_functions_test_double("double");
