46f09d67ffbf16725f1ffa13411111664fb632a5
Sven Mallach <sven.mallach@honei.org>
2007-06-19 21:48:07 +0000

    200! What is the present? Added new operations for MatrixSum and MatrixDifference.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@201 0f804a50-a62f-0410-84e1-872c020a11f9

---
+++ libla/matrix_difference.hh
@@ -0,0 +1,105 @@
+/* vim: set sw=4 sts=4 et nofoldenable : */
+
+/*
+ * Copyright (c) 2007 Sven Mallach <sven.mallach@uni-dortmund.de>
+ *
+ * This file is part of the LA C++ library. LibLa is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibLa is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef LIBLA_GUARD_MATRIX_DIFFERENCE_HH
+#define LIBLA_GUARD_MATRIX_DIFFERENCE_HH 1
+
+#include <libla/tags.hh>
+#include <libla/dense_matrix.hh>
+#include <libla/banded_matrix.hh>
+#include <libla/matrix_error.hh>
+
+/**
+ * \file
+ *
+ * Templatized definitions of matrix differences.<br/>
+ *
+ * \ingroup grpmatrixoperations
+ **/
+namespace pg512
+{
+
+    /**
+     * MatrixDifference is the class template for the difference of two matrix.
+     * \brief The first referenced matrix is changed under this operation.
+     * \ingroup grpmatrixoperations
+     **/
+    template <typename Tag_ = tags::CPU> struct MatrixDifference
+    {
+        /**
+         * Returns the the resulting matrix of the difference of two given DenseMatrix instances.
+         *
+         * \param left Reference to dense matrix that will be also used as result matrix.
+         * \param right Reference to constant dense matrix to be subtracted.
+         **/
+        template <typename DataType1_, typename DataType2_> static DenseMatrix<DataType1_> & value(DenseMatrix<DataType1_> & left, const DenseMatrix<DataType2_> & right)
+        {
+            if (left.columns() != right.columns())
+            {
+                throw MatrixColumnsDoNotMatch(right.columns(), left.columns());
+            }
+
+            if (left.rows() != left.rows())
+            {
+                throw MatrixRowsDoNotMatch(right.rows(), left.rows());
+            }
+
+            typename Matrix<DataType2_>::ConstElementIterator r(right.begin_elements());
+            for (typename MutableMatrix<DataType1_>::ElementIterator l(left.begin_elements()),
+                    l_end(left.end_elements()) ; l != l_end ; ++l)
+            {
+                *l -= *r;
+                r++;
+            }
+
+            return left;
+        }
+
+        /**
+         * Returns the the resulting matrix of the difference of two given BandedMatrix instances.
+         *
+         * \param left Reference to banded matrix that will be also used as result matrix.
+         * \param right Reference to constant banded matrix to be subtracted.
+         **/
+        template <typename DataType1_, typename DataType2_> static BandedMatrix<DataType1_> & value(BandedMatrix<DataType1_> & left, const BandedMatrix<DataType2_> & right)
+        {
+            if (left.columns() != right.columns())
+            {
+                throw MatrixColumnsDoNotMatch(right.columns(), left.columns());
+            }
+
+            if (left.rows() != left.rows())
+            {
+                throw MatrixRowsDoNotMatch(right.rows(), left.rows());
+            }
+
+            typename Matrix<DataType2_>::ConstElementIterator r(right.begin_elements());
+            for (typename MutableMatrix<DataType1_>::ElementIterator l(left.begin_elements()),
+                    l_end(left.end_elements()) ; l != l_end ; ++l)
+            {
+                *l -= *r;
+                r++;
+            }
+
+            return left;
+        }
+
+    };
+}
+#endif
