02eabf9473e649c7f40ea6609a339ff29f5fbf9e
Dirk Ribbrock <ribbrock@honei.org>
2007-06-19 15:56:29 +0000

    Oops, forgot to commit unittest directory.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@195 0f804a50-a62f-0410-84e1-872c020a11f9

--- unittest/unittest.hh
+++ unittest/unittest.hh
@@ -1,5 +1,25 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
+/*
+ * Copyright (c) 2007 Andre Matuschek <andre@matuschek.org>
+ * Copyright (c) 2007 David Gies      <david-gies@gmx.de> 
+ * Copyright (c) 2007 Danny van Dyk   <danny.dyk@uni-dortmund.de>
+ * Copyright (c) 2007 Dirk Ribbrock   <dirk.ribbrock@uni-dortmund.de>
+ *
+ * This file is part of the LA C++ library. LibLa is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibLa is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
 #ifndef UNITTEST_GUARD_UNITTEST_HH
 #define UNITTEST_GUARD_UNITTEST_HH 1
 
