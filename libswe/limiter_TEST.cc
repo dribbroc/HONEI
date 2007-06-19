d9b7189bfd7fdc0dde7319a7d187962411f91a1c
Dirk Ribbrock <ribbrock@honei.org>
2007-06-19 15:51:32 +0000

    Added GPL header to all test files.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@194 0f804a50-a62f-0410-84e1-872c020a11f9

--- libswe/limiter_TEST.cc
+++ libswe/limiter_TEST.cc
@@ -1,5 +1,24 @@
 /* vim: set number sw=4 sts=4 et foldmethod=syntax : */
 
+/*
+ * Copyright (c) 2007 Danny van Dyk <danny.dyk@uni-dortmund.de>
+ * Copyright (c) 2007 Volker Jung <volker.m.jung@t-online.de>
+ * Copyright (c) 2007 Dirk Ribbrock <dirk.ribbrock@uni-dortmund.de>
+ *
+ * This file is part of the SWE C++ library. LiSWE is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibSWE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
 #include <limiter.hh>
 #include <unittest/unittest.hh>
 
