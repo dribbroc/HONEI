3c9a9361d46f8715a5b4fc00134ad0657e784b41
Dirk Ribbrock <ribbrock@honei.org>
2007-06-21 08:58:47 +0000

    Added ScalarDenseMatrixSumTest.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@216 0f804a50-a62f-0410-84e1-872c020a11f9

---
+++ libla/scalar_matrix_sum_TEST.cc
@@ -0,0 +1,88 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2007 Danny van Dyk <danny.dyk@uni-dortmund.de>
+ * Copyright (c) 2007 Dirk Ribbrock <dirk.ribbrock@uni-dortmund.de>
+ *
+ * This file is part of the LA C++ library. LibLa is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibLa is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
+#include <libla/dense_matrix.hh>
+#include <libla/scalar_matrix_sum.hh>
+#include <unittest/unittest.hh>
+
+#include <limits>
+#include <tr1/memory>
+#include <iostream>
+
+using namespace pg512;
+using namespace tests;
+
+template <typename DataType_>
+class ScalarDenseMatrixSumTest :
+    public BaseTest
+{
+    public:
+        ScalarDenseMatrixSumTest(const std::string & type) :
+            BaseTest("scalar_dense_matrix_sum_test<" + type + ">")
+        {
+        }
+
+        virtual void run() const
+        {
+            for (unsigned long size(10) ; size < (1 << 12) ; size <<= 1)
+            {
+                DenseMatrix<DataType_> dm1(size, size + 1, DataType_(2)), dm2(size, size + 1, DataType_(5));
+                DenseMatrix<DataType_> & prod(ScalarMatrixSum<DataType_>::value(DataType_(3), dm1));
+
+                for (typename Matrix<DataType_>::ConstElementIterator i(dm1.begin_elements()),
+                        i_end(dm1.end_elements()), j(dm2.begin_elements()) ; i != i_end ; ++i, ++j)
+                {
+                    TEST_CHECK_EQUAL_WITHIN_EPS(*i, *j, std::numeric_limits<DataType_>::epsilon());
+                }
+            }
+        }
+};
+
+ScalarDenseMatrixSumTest<float> scalar_dense_matrix_sum_test_float("float");
+ScalarDenseMatrixSumTest<double> scalar_dense_matrix_sum_test_double("double");
+
+template <typename DataType_>
+class ScalarDenseMatrixSumQuickTest :
+    public QuickTest
+{
+    public:
+        ScalarDenseMatrixSumQuickTest(const std::string & type) :
+            QuickTest("scalar_dense_matrix_sum_quick_test<" + type + ">")
+        {
+        }
+
+        virtual void run() const
+        {
+            unsigned long size(5);
+            DenseMatrix<DataType_> dm(size, size + 1, DataType_(2));
+            DenseMatrix<DataType_> prod1(ScalarMatrixSum<DataType_>::value(DataType_(3), dm));
+
+            DataType_ vsum(0), ssum(2 * DataType_(size) * DataType_(size + 1));
+            for (typename MutableMatrix<DataType_>::ElementIterator i(dm.begin_elements()),
+                    i_end(dm.end_elements()) ; i != i_end ; ++i)
+            {
+                vsum += *i;
+            }
+            TEST_CHECK_EQUAL_WITHIN_EPS(vsum, static_cast<DataType_>(5 * size * (size +1)), 
+                std::numeric_limits<DataType_>::epsilon());
+        }
+};
+ScalarDenseMatrixSumQuickTest<float> scalar_dense_matrix_sum_quick_test_float("float");
+ScalarDenseMatrixSumQuickTest<double> scalar_dense_matrix_sum_quick_test_double("double");
