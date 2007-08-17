cdebeaa7549b92da8358b39d0c72236417cca68e
Danny van Dyk <dyk@honei.org>
2007-08-17 14:13:34 +0000

    Oops. Forgot to add test cases.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@505 0f804a50-a62f-0410-84e1-872c020a11f9

---
+++ libutil/spe_manager_TEST.cc
@@ -0,0 +1,43 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+// This test case needs debug support.
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+
+#include <libutil/spe_manager.hh>
+#include <unittest/unittest.hh>
+
+#include <iostream>
+
+using namespace pg512;
+using namespace tests;
+
+class SPEManagerIterationTest :
+    public QuickTest
+{
+    public:
+        SPEManagerIterationTest() :
+            QuickTest("spe_manager_iteration_test")
+        {
+        }
+
+        virtual void run() const
+        {
+	    try
+            {
+                unsigned long count(0);
+                for (SPEManager::Iterator i(SPEManager::instance()->begin()), i_end(SPEManager::instance()->end()) ;
+                        i != i_end ; ++i, ++count)
+                {
+                    std::cout << "SPE id is '" << i->id() << "'" << std::endl;
+                }
+                std::cout << "Number of SPEs is " << count << std::endl;
+                TEST_CHECK(true);
+            }
+            catch (...)
+            {
+                TEST_CHECK(false);
+            }
+        }
+} spe_manager_iteration_test;
