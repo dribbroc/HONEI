9fefc71a149ade49b332e0630608e0f8e012bac0
Markus Geveler <apryde@gmx.de>
2007-08-27 16:58:45 +0000

    Can you see the wave?
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@571 0f804a50-a62f-0410-84e1-872c020a11f9

--- libswe/solver_TEST.cc
+++ libswe/solver_TEST.cc
@@ -41,15 +41,15 @@ class RelaxSolverQuickTest :
 
         virtual void run() const
         {
-            ulint dwidth =4;
-            ulint dheight =4;
+            ulint dwidth =16;
+            ulint dheight =16;
             ulint timesteps =1;
  
             DenseMatrix<DataType_> height(dheight, dwidth, DataType_(5));
             //SCENARIO setup
             for(ulint i = 0; i< height.rows(); ++i)
             {
-                (height)[0][i] = 5;
+                (height)[0][i] = 10;
             }
             //END SCENARIO setup
             DenseMatrix<DataType_> bottom(dheight, dwidth, DataType_(1));
@@ -61,12 +61,12 @@ class RelaxSolverQuickTest :
             DenseVector<DataType_> w(entries, DataType_(1)); 
             DenseVector<DataType_> bx (entries/3, DataType_(0));
             DenseVector<DataType_> by (entries/3, DataType_(0));
-            DenseVector<DataType_> c (3,DataType_(0.00001));
-            DenseVector<DataType_> d (3,DataType_(0.00001));
+            DenseVector<DataType_> c (3,DataType_(0.000001));
+            DenseVector<DataType_> d (3,DataType_(0.000001));
             
-            DataType_ deltax = .2;                       
-            DataType_ deltay = .2;
-            DataType_ deltat = .2;
+            DataType_ deltax = .01;                       
+            DataType_ deltay = .01;
+            DataType_ deltat = .01;
 
             double eps = 1e-200;                  
             RelaxSolver<DataType_, DataType_, DataType_, DataType_, DataType_> relax_solver
