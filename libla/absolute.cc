7acf160db178a9ee79f7ba841e26138a14e699d7
Danny van Dyk <dyk@honei.org>
2007-08-31 14:48:01 +0000

    Forgot to add files to SVN.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@592 0f804a50-a62f-0410-84e1-872c020a11f9

---
+++ libla/absolute.cc
@@ -0,0 +1,83 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2007 Danny van Dyk <danny.dyk@uni-dortmund.de>
+ *
+ * This file is part of the LA C++ library. LibLa is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibLa is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <libla/absolute.hh>
+
+using namespace honei;
+
+template <>
+DenseVector<float> &
+Absolute<tags::CPU>::value(DenseVector<float> & x)
+{
+    CONTEXT("When calculating the absolute value of DenseVector<float> elements");
+
+    for (Vector<float>::ElementIterator i(x.begin_elements()), i_end(x.end_elements()) ;
+            i != i_end ; ++i)
+    {
+        *i = fabs(*i);
+    }
+
+    return x;
+}
+
+template <>
+SparseVector<float> &
+Absolute<tags::CPU>::value(SparseVector<float> & x)
+{
+    CONTEXT("When calculating the absolute value of SparseVector<float> elements");
+
+    for (Vector<float>::ElementIterator i(x.begin_non_zero_elements()), i_end(x.end_non_zero_elements()) ;
+            i != i_end ; ++i)
+    {
+        *i = fabs(*i);
+    }
+
+    return x;
+}
+
+template <>
+DenseVector<double> &
+Absolute<tags::CPU>::value(DenseVector<double> & x)
+{
+    CONTEXT("When calculating the absolute value of DenseVector<double> elements");
+
+    for (Vector<double>::ElementIterator i(x.begin_elements()), i_end(x.end_elements()) ;
+            i != i_end ; ++i)
+    {
+        *i = fabs(*i);
+    }
+
+    return x;
+}
+
+template <>
+SparseVector<double> &
+Absolute<tags::CPU>::value(SparseVector<double> & x)
+{
+    CONTEXT("When calculating the absolute value of SparseVector<double> elements");
+
+    for (Vector<double>::ElementIterator i(x.begin_non_zero_elements()), i_end(x.end_non_zero_elements()) ;
+            i != i_end ; ++i)
+    {
+        *i = fabs(*i);
+    }
+
+    return x;
+}
+
