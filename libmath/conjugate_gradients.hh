4094f4a82c76668b62f88a734f9964e75647ae5c
Markus Geveler <apryde@gmx.de>
2007-10-01 19:39:17 +0000

    Added implementation of CG solver for banded and dense systems and its test.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@688 0f804a50-a62f-0410-84e1-872c020a11f9

---
+++ libmath/conjugate_gradients.hh
@@ -0,0 +1,284 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2007 Markus Geveler <apryde@gmx.de>
+ *
+ * This file is part of the MATH C++ library. LibMath is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibMath is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef LIBMATH_GUARD_CONJUGATE_GRADIENTS_HH
+#define LIBMATH_GUARD_CONJUGATE_GRADIENTS_HH 1
+
+#include <libutil/tags.hh>
+#include <libla/dense_matrix.hh>
+#include <libla/dense_vector.hh>
+#include <libla/product.hh>
+#include <libla/sum.hh>
+#include <libla/difference.hh>
+#include <libla/dot_product.hh>
+#include <libla/scale.hh>
+#include <libla/norm.hh>
+#include <iostream>
+
+/**
+ * \file
+ *
+ * Templatized definition and implementation of a Conjugate Gradients Solver.
+ *
+ * \ingroup grpoperations
+ */
+
+using namespace std;
+namespace honei
+{
+    /**
+     * \brief Solution of LES with CG.
+     *
+     * The referenced containers are invariant under this operation.
+     * In every case, a new object is created and returned.
+     *
+     * \ingroup grpmatrixoperations
+     * \ingroup grpvectoroperations
+     */
+
+    template <typename Tag_=tags::CPU>
+    struct ConjugateGradients
+    {
+        private:
+            template<typename DT1_, typename DT2_>
+            static inline void cg_kernel(DenseMatrix<DT1_> & system_matrix, DenseVector<DT2_> & right_hand_side, DenseVector<DT1_> & former_gradient, DenseVector<DT1_> & former_result, DenseVector<DT1_> & utility)
+            {
+                ///Compute x_i+1: (in energyand implementation of a Conjugate Gradients Solver)
+                DT1_ upper = DotProduct<Tag_>::value(former_gradient, former_gradient);
+                DenseVector<DT1_> energy = Product<Tag_>::value(system_matrix, utility);
+                DT1_ lower = DotProduct<Tag_>::value(energy, utility);
+                DenseVector<DT1_> u_c(utility.copy());
+                energy = Scale<Tag_>::value(DT1_(upper/lower), u_c);
+                Sum<Tag_>::value(energy, former_result);
+                ///Compute new gradient
+                DenseVector<DT1_> new_gradient = Product<Tag_>::value(system_matrix, energy);
+                Difference<Tag_>::value(new_gradient, right_hand_side);
+
+                ///Compute new utility
+                upper = DotProduct<Tag_>::value(new_gradient, new_gradient);
+                lower = DotProduct<Tag_>::value(former_gradient, former_gradient);
+                Scale<Tag_>::value(DT1_(upper/lower), utility);
+                Difference<Tag_>::value(utility, new_gradient);
+
+                ///Finishing:
+                former_gradient = new_gradient.copy();
+                former_result = energy.copy();
+
+            }
+
+            template<typename DT1_, typename DT2_>
+            static inline void cg_kernel(BandedMatrix<DT1_> & system_matrix, DenseVector<DT2_> & right_hand_side, DenseVector<DT1_> & former_gradient, DenseVector<DT1_> & former_result, DenseVector<DT1_> & utility)
+            {
+                ///Compute x_i+1: (in energy)
+                DT1_ upper = DotProduct<Tag_>::value(former_gradient, former_gradient);
+                DenseVector<DT1_> energy = Product<Tag_>::value(system_matrix, utility);
+                DT1_ lower = DotProduct<Tag_>::value(energy, utility);
+                DenseVector<DT1_> u_c(utility.copy());
+                energy = Scale<Tag_>::value(DT1_(upper/lower), u_c);
+                Sum<Tag_>::value(energy, former_result);
+                ///Compute new gradient
+                DenseVector<DT1_> new_gradient = Product<Tag_>::value(system_matrix, energy);
+                Difference<Tag_>::value(new_gradient, right_hand_side);
+
+                ///Compute new utility
+                upper = DotProduct<Tag_>::value(new_gradient, new_gradient);
+                lower = DotProduct<Tag_>::value(former_gradient, former_gradient);
+                Scale<Tag_>::value(DT1_(upper/lower), utility);
+                Difference<Tag_>::value(utility, new_gradient);
+
+                ///Finishing:
+                former_gradient = new_gradient.copy();
+                former_result = energy.copy();
+
+            }
+
+            template<typename DT1_, typename DT2_>
+            static inline void cg_kernel(SparseMatrix<DT1_> & system_matrix, DenseVector<DT2_> & right_hand_side, DenseVector<DT1_> & former_gradient, DenseVector<DT1_> & former_result, DenseVector<DT1_> & utility)
+            {
+                ///Compute x_i+1: (in energy)
+                DT1_ upper = DotProduct<Tag_>::value(former_gradient, former_gradient);
+                SparseVector<DT1_> energy = Product<Tag_>::value(system_matrix, utility);
+                DT1_ lower = DotProduct<Tag_>::value(energy, utility);
+                DenseVector<DT1_> u_c(utility.copy());
+                energy = Scale<Tag_>::value(DT1_(upper/lower), u_c);
+                Sum<Tag_>::value(energy, former_result);
+                ///Compute new gradient
+                SparseVector<DT1_> new_gradient = Product<Tag_>::value(system_matrix, energy);
+                Difference<Tag_>::value(new_gradient, right_hand_side);
+
+                ///Compute new utility
+                upper = DotProduct<Tag_>::value(new_gradient, new_gradient);
+                lower = DotProduct<Tag_>::value(former_gradient, former_gradient);
+                Scale<Tag_>::value(DT1_(upper/lower), utility);
+                Difference<Tag_>::value(utility, new_gradient);
+
+                ///Finishing:
+                former_gradient = new_gradient.copy();
+                former_result = energy.copy();
+
+            }
+
+
+
+
+        public:
+            /**
+            * \brief Returns solution of LES given by a DenseMatrix and a Vector.
+            *
+            * \param system_matrix The system matrix.
+            * \param right_hand_side The right hand side of the system.
+            * \param iter_number The fixed number of iterations.
+            *
+           */
+
+            /// \{
+
+
+            template <typename DT1_, typename DT2_>
+            static DenseVector<DT1_> value(DenseMatrix<DT1_> & system_matrix, DenseVector<DT2_> & right_hand_side,long iter_number)
+            {
+                CONTEXT("When solving dense linear system with CG (fixed # iterations):");
+
+
+                DenseVector<DT1_> x(right_hand_side.size(), DT1_(0));
+                DenseVector<DT1_> g = Product<Tag_>::value(system_matrix, x);
+                Difference<DT1_>::value(g, right_hand_side);
+                DenseVector<DT1_> g_c(g.copy());
+                DenseVector<DT1_> u = Scale<>::value(DT1_(-1.), g_c);
+                for(unsigned long i = 0; i<iter_number; ++i)
+                {
+                    cg_kernel(system_matrix, right_hand_side, g, x, u);
+                }
+                return x;
+            }
+            /**
+            * \brief Returns solution of LES given by a DenseMatrix and a Vector.
+            *
+            * \param system_matrix The system matrix.
+            * \param right_hand_side The right hand side of the system.
+            * \param konv_rad The parameter for convergence control.
+            *
+            */
+
+            /// \{
+
+
+            template <typename DT1_, typename DT2_>
+            static DenseVector<DT1_> value(DenseMatrix<DT1_> & system_matrix, DenseVector<DT2_> & right_hand_side, double konv_rad)
+            {
+                CONTEXT("When solving dense linear system with CG (with given convergence parameter):");
+
+
+                DenseVector<DT1_> x(right_hand_side.size(), DT1_(0));
+                DenseVector<DT1_> g = Product<Tag_>::value(system_matrix, x);
+                Difference<DT1_>::value(g, right_hand_side);
+                DenseVector<DT1_> g_c(g.copy());
+                DenseVector<DT1_> u = Scale<>::value(DT1_(-1.), g_c);
+                DenseVector<DT1_> x_last(x.copy());
+
+                DT1_ norm_x_last = DT1_(0);
+                DT1_ norm_x = DT1_(1);
+
+                while(norm_x - norm_x_last > konv_rad)
+                {
+
+                    cg_kernel(system_matrix, right_hand_side, g, x, u);
+                    norm_x = Norm<vnt_l_two, false, Tag_>::value(x);
+                    norm_x_last = Norm<vnt_l_two, false, Tag_>::value(x_last);
+                    x_last = x.copy();
+                }
+                return x;
+
+
+            }
+            /**
+            * \brief Returns solution of LES given by a BandedMatrix and a Vector.
+            *
+            * \param system_matrix The system matrix.
+            * \param right_hand_side The right hand side of the system.
+            * \param iter_number The fixed number of iterations.
+            *
+            */
+
+            /// \{
+
+
+
+            template <typename DT1_, typename DT2_>
+            static DenseVector<DT1_> value(BandedMatrix<DT1_> & system_matrix, DenseVector<DT2_> & right_hand_side,long iter_number)
+            {
+                CONTEXT("When solving banded linear system with CG (with fixed # of iterations):");
+
+
+
+                DenseVector<DT1_> x(right_hand_side.size(), DT1_(0));
+                DenseVector<DT1_> g = Product<Tag_>::value(system_matrix, x);
+                Difference<DT1_>::value(g, right_hand_side);
+                DenseVector<DT1_> g_c(g.copy());
+                DenseVector<DT1_> u = Scale<>::value(DT1_(-1.), g_c);
+                for(unsigned long i = 0; i<iter_number; ++i)
+                {
+                    cg_kernel(system_matrix, right_hand_side, g, x, u);
+                }
+                return x;
+            }
+            /**
+            * \brief Returns solution of LES given by a BandedMatrix and a Vector.
+            *
+            * \param system_matrix The system matrix.
+            * \param right_hand_side The right hand side of the system.
+            * \param konv_rad The parameter for convergence control.
+            *
+            */
+
+            /// \{
+
+
+            template <typename DT1_, typename DT2_>
+            static DenseVector<DT1_> value(BandedMatrix<DT1_> & system_matrix, DenseVector<DT2_> & right_hand_side, double konv_rad)
+            {
+                CONTEXT("When solving banded linear system with CG (with given convergence parameter):");
+
+
+                DenseVector<DT1_> x(right_hand_side.size(), DT1_(0));
+                DenseVector<DT1_> g = Product<Tag_>::value(system_matrix, x);
+                Difference<DT1_>::value(g, right_hand_side);
+                DenseVector<DT1_> g_c(g.copy());
+                DenseVector<DT1_> u = Scale<>::value(DT1_(-1.), g_c);
+                DenseVector<DT1_> x_last(x.copy());
+
+                DT1_ norm_x_last = DT1_(0);
+                DT1_ norm_x = DT1_(1);
+
+                while(norm_x - norm_x_last > konv_rad)
+                {
+
+                    cg_kernel(system_matrix, right_hand_side, g, x, u);
+                    norm_x = Norm<vnt_l_two, false, Tag_>::value(x);
+                    norm_x_last = Norm<vnt_l_two, false, Tag_>::value(x_last);
+                    x_last = x.copy();
+                }
+                return x;
+
+
+            }
+   };
+}
+
+#endif
