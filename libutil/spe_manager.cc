defc769547fb42fdcddaa517e9a18c3e7c9ccd24
Dirk Ribbrock <ribbrock@honei.org>
2007-10-28 16:33:00 +0000

    Removed cout.flush().
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@793 0f804a50-a62f-0410-84e1-872c020a11f9

--- libutil/spe_manager.cc
+++ libutil/spe_manager.cc
@@ -64,7 +64,7 @@ struct SPEManager::Implementation
 
         /// \todo Find matching kernel, enqueue with that kernel
         //Round Robin dispatching
-        std::cout << "SPEManager: Dispatching to spe "<< next_spe - spe_list.begin()<<" with load "<<next_spe->kernel()->instruction_load()<<std::endl;
+        std::cout << "SPEManager: Dispatching to spe " << next_spe - spe_list.begin() << " with load " << next_spe->kernel()->instruction_load() << std::endl;
         instruction.enqueue_with(next_spe->kernel());
         next_spe ++;
         if (next_spe == spe_list.end())
@@ -98,7 +98,6 @@ SPEManager::SPEManager() :
     {
         SPE spe;
         _imp->spe_list.push_back(spe);
-        std::cout.flush();
     }
     for (std::vector<SPE>::iterator i(_imp->spe_list.begin()) ; i != _imp->spe_list.end() ; i++)
     {
