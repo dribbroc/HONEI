8b856d02d53e673d1e6526e7ef14108ba8cef104
Volker Jung <volker.jung@uni-dortmund.de>
2007-11-02 12:37:23 +0000

    Added ThreadPool implementation.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@812 0f804a50-a62f-0410-84e1-872c020a11f9

---
+++ libutil/pool_thread.cc
@@ -0,0 +1,148 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2007 Volker Jung <volker.jung@uni-dortmund.de>
+ *
+ * This file is part of the Utility C++ library. LibUtil is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibUtil is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <libutil/condition_variable.hh>
+#include <libutil/exception.hh>
+#include <libutil/lock.hh>
+#include <libutil/mutex.hh>
+#include <libutil/stringify.hh>
+#include <libutil/pool_task.hh>
+#include <libutil/pool_thread.hh>
+#include <libutil/thread_pool.hh>
+
+#include <list>
+
+#include <cerrno>
+
+using namespace honei;
+
+struct PoolThread::Implementation
+{
+    private:
+        /// Our thread.
+        pthread_t * const _thread;
+
+        PoolThread * const _pool_thread;
+
+        /// Our mutex for read/write access to a PoolThread instance.
+        Mutex * const _mutex;
+
+        /// Our condition variable.
+        ConditionVariable * const _wake_up;
+
+        /// Our exit condition.
+        bool _exit;
+
+        /// Our actual task.
+        PoolTask * _task;
+
+        bool _task_has_run;
+
+        /// The pool we belong to.
+        ThreadPool * const _pool;
+
+    public:
+        /// Set task and wake up this thread for computation.
+        inline void run(PoolTask * task)
+        {
+            Lock l(*_mutex);
+
+            _task = task;
+            _task_has_run = false;
+
+            _wake_up->signal(); //Notify this thread that new work is waiting.
+        }
+
+        /// Our thread's main function.
+        static void * thread_function(void * argument)
+        {
+            Implementation * imp(static_cast<Implementation *>(argument));
+            bool exit(false);
+            do
+            {
+                Lock l(*imp->_mutex);
+
+                // If there is no task to be run, wait
+                if (imp->_task_has_run)
+                {
+                    imp->_wake_up->wait(*imp->_mutex);
+                }
+
+                // Update our exit condition
+                exit = imp->_exit;
+
+                // Run our task
+                if (! imp->_task_has_run)
+                {
+                    (*imp->_task)();
+                    imp->_task_has_run = true;
+                    imp->_pool->notify(imp->_pool_thread);
+                }
+            }
+            while (! exit);
+
+            pthread_exit(0);
+        }
+
+        Implementation(ThreadPool * pool, PoolThread * pool_thread) :
+            _thread(new pthread_t),
+            _pool_thread(pool_thread),
+            _mutex(new Mutex),
+            _exit(false),
+            _pool(pool),
+            _task_has_run(true),
+            _wake_up(new ConditionVariable),
+            _task(0)
+        {
+            int retval;
+            if (0 != (retval = pthread_create(_thread, 0, &thread_function, this)))
+                throw ExternalError("libpthread", "pthread_create failed, " + stringify(strerror(retval)));
+        }
+
+        ~Implementation()
+        {
+            // Flag for exit.
+            {
+                Lock l(*_mutex);
+                _exit = true;
+                _wake_up->signal();
+            }
+
+            pthread_join(*_thread, 0);
+
+            delete _mutex;
+            delete _thread;
+        }
+};
+
+PoolThread::PoolThread(ThreadPool * pool) :
+    _imp(new Implementation(pool, this))
+{
+}
+
+PoolThread::~PoolThread()
+{
+    delete _imp;
+}
+
+void
+PoolThread::run(PoolTask * task)
+{
+    _imp->run(task);
+}
---
+++ libutil/thread_pool.hh
@@ -0,0 +1,143 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2007 Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
+ * Copyright (c) 2007 Volker Jung <volker.jung@uni-dortmund.de>
+ *
+ * This file is part of the Utility C++ library. LibUtil is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibUtil is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef LIBUTIL_GUARD_THREAD_POOL_HH
+#define LIBUTIL_GUARD_THREAD_POOL_HH 1
+
+#include <libutil/condition_variable.hh>
+#include <libutil/lock.hh>
+#include <libutil/mutex.hh>
+#include <libutil/pool_task.hh>
+#include <libutil/pool_thread.hh>
+#include <libutil/stringify.hh>
+
+#include <list>
+#include <tr1/functional>
+
+#define DEFAULT_NUM_THREADS 4
+
+using namespace std;
+
+namespace honei
+{
+    typedef std::tr1::function<void () throw ()> WorkerTask;
+
+    class ThreadPool
+    {
+        private:
+            ///Our threads.
+            std::list<PoolThread *> _pool;
+
+            ///Our list of idle threads.
+            std::list<PoolThread *> _idle_threads;
+
+            ///Our list of untreated tasks.
+            std::list<PoolTask *> _task_list;
+
+            ///Our mutex.
+            Mutex * const _mutex;
+
+            ///Our condition variable.
+            ConditionVariable * const _available;
+
+            /**
+             * Constructor.
+             *
+             * \param num_threads Number of threads the threadpool consists of.
+             */
+            ThreadPool(unsigned long num_threads) :
+                _mutex(new Mutex),
+                _available(new ConditionVariable)
+            {
+                for (unsigned long i(0); i < num_threads; ++i)
+                {
+                    PoolThread * poolthread(new PoolThread(this));
+                    _pool.push_back(poolthread);
+                    _idle_threads.push_back(poolthread);
+                }
+            }
+
+
+        public:
+
+            /// Returns the singleton instance of the pool.
+            //If it doesn't exist yet, it is created with numThreads threads. numThreads is ignored otherwise.
+            static ThreadPool * get_instance(unsigned long num_threads)
+            {
+                static ThreadPool result(num_threads);
+                return &result;
+            }
+
+            /// Returns the singleton instance of the pool.
+            //If it doesn't exist yet, it is created with a default number of threads. Useful for convenience.
+            static ThreadPool * get_instance()
+            {
+                static ThreadPool result(DEFAULT_NUM_THREADS);
+                return &result;
+            }
+
+           ///Destructor.
+            ~ThreadPool()
+            {
+                delete _mutex;
+                delete _available;
+            }
+
+            ///Processes incoming tasks.
+            PoolTask * dispatch(WorkerTask task)
+            {
+                Lock l(*_mutex);
+
+                PoolTask * pt(new PoolTask(task));
+                if(! _idle_threads.empty())
+                {
+                    //Fetch idle thread and dispatch task.
+                    PoolThread * result = _idle_threads.front();
+                    _idle_threads.pop_front();
+                    result->run(pt);
+                }
+                else
+                {
+                    //Since there is no idle thread, enqueue task.
+                    _task_list.push_back(pt);
+                }
+                return pt;
+            }
+
+            void notify(PoolThread * returning_thread)
+            {
+                Lock l(*_mutex);
+
+                if(! _task_list.empty())
+                {
+                    //Fetch enqueued task and dispatch to returning thread.
+                    PoolTask * next_task = _task_list.front();
+                    _task_list.pop_front();
+                    returning_thread->run(next_task);
+                }
+                else
+                {
+                    //Enqueue returning thread in our idle list.
+                    _idle_threads.push_back(returning_thread);
+                }
+            }
+    };
+}
+#endif
