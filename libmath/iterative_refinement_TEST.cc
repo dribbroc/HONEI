51edfc17e36af5e416ea3df2c007f483159165f4
Markus Geveler <apryde@gmx.de>
2007-11-06 10:59:51 +0000

    Added implementation of iterative refinement for banded systems, resolved minor conversion bug.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@844 0f804a50-a62f-0410-84e1-872c020a11f9

--- libmath/iterative_refinement_TEST.cc
+++ libmath/iterative_refinement_TEST.cc
@@ -114,6 +114,139 @@ class IterativeRefinementTestDenseJAC:
         }
 };
 
+template <typename Tag_, typename DT1_>
+class IterativeRefinementTestBandedCG:
+    public BaseTest
+{
+    public:
+        IterativeRefinementTestBandedCG(const std::string & tag) :
+            BaseTest("Iterative Refinement with CG solver test (Banded system)<" + tag + ">")
+        {
+        }
+
+        virtual void run() const
+        {
+
+            DenseVector<DT1_> diag(3, DT1_(1));
+            DenseVector<DT1_> bp1(3, DT1_(1));
+            DenseVector<DT1_> bp2(3, DT1_(1));
+            DenseVector<DT1_> bm1(3, DT1_(1));
+            DenseVector<DT1_> bm2(3, DT1_(1));
+
+            diag[0] = DT1_(7);
+            diag[1] = DT1_(6);
+            diag[2] = DT1_(5);
+            bp1[0] = DT1_(-2);
+            bp1[1] = DT1_(2);
+            bp1[2] = DT1_(0);
+            bp2[0] = DT1_(0);
+            bp2[1] = DT1_(0);
+            bp2[2] = DT1_(0);
+            bm1[0] = DT1_(0);
+            bm1[1] = DT1_(-2);
+            bm1[2] = DT1_(2);
+            bm2[0] = DT1_(0);
+            bm2[1] = DT1_(0);
+            bm2[2] = DT1_(0);
+
+            BandedMatrix<DT1_> A(3);
+            A.insert_band(0, diag);
+            A.insert_band(1, bp1);
+            A.insert_band(2, bp2);
+            A.insert_band(-1, bm1);
+            A.insert_band(-2, bm2);
+
+            DenseVector<DT1_> b(3, DT1_(1));
+            b[0] = DT1_(3);
+            b[1] = DT1_(3);
+            b[2] = DT1_(0);
+
+
+            std::cout<<"A:"<<A<<endl;
+            std::cout<<"b:"<<b<<endl;
+            DenseVector<DT1_> result(IterativeRefinement<CG, tags::CPU>::value(A,b,double(0.00001), double(0.00001)));
+            DT1_ x_n = Norm< vnt_l_two, false, DT1_>::value(result);
+            DenseVector<DT1_> x_analytical(3, DT1_(0));
+            cout<<"RESULT(v1):"<<result<<endl;
+
+            x_analytical[0] = DT1_(2./3.);
+            x_analytical[1] = DT1_(5./6.);
+            x_analytical[2] = DT1_(-1./3.);
+            DT1_ x_analytical_n = Norm< vnt_l_two, false, DT1_>::value(x_analytical);
+            TEST_CHECK_EQUAL_WITHIN_EPS(x_analytical_n, x_n , double(0.001));
+
+        }
+};
+
+template <typename Tag_, typename DT1_>
+class IterativeRefinementTestBandedJacobi:
+    public BaseTest
+{
+    public:
+        IterativeRefinementTestBandedJacobi(const std::string & tag) :
+            BaseTest("Iterative Refinement with JAC solver test (Banded system)<" + tag + ">")
+        {
+        }
+
+        virtual void run() const
+        {
+
+            DenseVector<DT1_> diag(3, DT1_(1));
+            DenseVector<DT1_> bp1(3, DT1_(1));
+            DenseVector<DT1_> bp2(3, DT1_(1));
+            DenseVector<DT1_> bm1(3, DT1_(1));
+            DenseVector<DT1_> bm2(3, DT1_(1));
+
+            diag[0] = DT1_(7);
+            diag[1] = DT1_(6);
+            diag[2] = DT1_(5);
+            bp1[0] = DT1_(-2);
+            bp1[1] = DT1_(2);
+            bp1[2] = DT1_(0);
+            bp2[0] = DT1_(0);
+            bp2[1] = DT1_(0);
+            bp2[2] = DT1_(0);
+            bm1[0] = DT1_(0);
+            bm1[1] = DT1_(-2);
+            bm1[2] = DT1_(2);
+            bm2[0] = DT1_(0);
+            bm2[1] = DT1_(0);
+            bm2[2] = DT1_(0);
+
+            BandedMatrix<DT1_> A(3);
+            A.insert_band(0, diag);
+            A.insert_band(1, bp1);
+            A.insert_band(2, bp2);
+            A.insert_band(-1, bm1);
+            A.insert_band(-2, bm2);
+
+            DenseVector<DT1_> b(3, DT1_(1));
+            b[0] = DT1_(3);
+            b[1] = DT1_(3);
+            b[2] = DT1_(0);
+
+
+            std::cout<<"A:"<<A<<endl;
+            std::cout<<"b:"<<b<<endl;
+            DenseVector<DT1_> result(IterativeRefinement<JAC, tags::CPU>::value(A,b,double(0.0000001), double(0.0000001)));
+            DT1_ x_n = Norm< vnt_l_two, false, DT1_>::value(result);
+            DenseVector<DT1_> x_analytical(3, DT1_(0));
+            cout<<"RESULT(v1):"<<result<<endl;
+
+            x_analytical[0] = DT1_(2./3.);
+            x_analytical[1] = DT1_(5./6.);
+            x_analytical[2] = DT1_(-1./3.);
+            DT1_ x_analytical_n = Norm< vnt_l_two, false, DT1_>::value(x_analytical);
+            TEST_CHECK_EQUAL_WITHIN_EPS(x_analytical_n, x_n , double(0.001));
+
+        }
+};
+
+
+
 IterativeRefinementTestDenseCG<tags::CPU, double> iterref_test_double_denseCG("double");
 IterativeRefinementTestDenseJAC<tags::CPU, double> iterref_test_double_denseJAC("double");
+
+IterativeRefinementTestBandedCG<tags::CPU, double> iterref_test_double_bandedCG("double");
+IterativeRefinementTestBandedJacobi<tags::CPU, double> iterref_test_double_bandedJAC("double");
 
