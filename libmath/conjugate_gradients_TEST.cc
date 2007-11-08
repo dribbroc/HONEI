50fbee9923d3b07ba75ebe0cda13e89feaa09c90
Markus Geveler <apryde@gmx.de>
2007-11-08 20:21:37 +0000

    Added implementation of JACOBI - preconditioned CG and its test (both for dense systems).
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@857 0f804a50-a62f-0410-84e1-872c020a11f9

--- libmath/conjugate_gradients_TEST.cc
+++ libmath/conjugate_gradients_TEST.cc
@@ -146,14 +146,72 @@ class ConjugateGradientsTestBanded:
         }
 };
 
+template <typename Tag_, typename DT1_>
+class ConjugateGradientsTestDenseJAC:
+    public BaseTest
+{
+    public:
+        ConjugateGradientsTestDenseJAC(const std::string & tag) :
+            BaseTest("Preconditioned (JAC) Conjugate gradients solver test (Dense system)<" + tag + ">")
+        {
+            register_tag(Tag_::name);
+        }
+
+        virtual void run() const
+        {
+            DenseMatrix<DT1_> A(3, 3, DT1_(1));
+            DenseVector<DT1_> b(3, DT1_(1));
+            A[0][0] = DT1_(7);
+            A[0][1] = DT1_(-2);
+            A[0][2] = DT1_(0);
+            A[1][0] = DT1_(-2);
+            A[1][1] = DT1_(6);
+            A[1][2] = DT1_(2);
+            A[2][0] = DT1_(0);
+            A[2][1] = DT1_(2);
+            A[2][2] = DT1_(5);
+
+            b[0] = DT1_(3);
+            b[1] = DT1_(3);
+            b[2] = DT1_(0);
+
+            std::cout<<"A:"<<A<<endl;
+            std::cout<<"b:"<<b<<endl;
+            //DenseVector<DT1_> result = ConjugateGradients<tags::CPU, methods::JAC>::value(A,b,long(2));
+            //DT1_ x_n = Norm< vnt_l_two, false, DT1_>::value(result);
+            DenseVector<DT1_> x_analytical(3, DT1_(0));
+            //cout<<"RESULT(v1):"<<result<<endl;
+            x_analytical[0] = DT1_(2./3.);
+            x_analytical[1] = DT1_(5./6.);
+            x_analytical[2] = DT1_(-1./3.);
+            DT1_ x_analytical_n = Norm< vnt_l_two, false, DT1_>::value(x_analytical);
+            //TEST_CHECK_EQUAL_WITHIN_EPS(x_analytical_n, x_n , double(0.1));
+
+            DenseVector<DT1_> result_2 = ConjugateGradients<tags::CPU, methods::JAC>::value(A,b,double(0.00001));
+            cout<<"RESULT(v2):"<<result_2<<endl;
+
+            DT1_ x_n_2 = Norm< vnt_l_two, false, DT1_>::value(result_2);
+            TEST_CHECK_EQUAL_WITHIN_EPS(x_analytical_n, x_n_2 , double(0.1));
+
+        }
+};
+
+
 
 ConjugateGradientsTestDense<tags::CPU, float> cg_test_float_dense("float");
 ConjugateGradientsTestDense<tags::CPU, double> cg_test_double_dense("double");
 ConjugateGradientsTestBanded<tags::CPU, float> cg_test_float_banded("float");
 ConjugateGradientsTestBanded<tags::CPU, double> cg_test_double_banded("double");
+ConjugateGradientsTestDenseJAC<tags::CPU, float> cg_test_float_dense_jac("float");
+ConjugateGradientsTestDenseJAC<tags::CPU, double> cg_test_double_dense_jac("double");
+
 #ifdef HONEI_SSE
 ConjugateGradientsTestDense<tags::CPU::SSE, float> sse_cg_test_float_dense("SSE float");
 ConjugateGradientsTestDense<tags::CPU::SSE, double> sse_cg_test_double_dense("SSE double");
 ConjugateGradientsTestBanded<tags::CPU::SSE, float> sse_cg_test_float_banded("SSE float");
 ConjugateGradientsTestBanded<tags::CPU::SSE, double> sse_cg_test_double_banded("SSE double");
+ConjugateGradientsTestDenseJAC<tags::CPU::SSE, float> sse_cg_test_float_dense_jac("float");
+ConjugateGradientsTestDenseJAC<tags::CPU::SSE, double> sse_cg_test_double_dense_jac("double");
+
+
 #endif
