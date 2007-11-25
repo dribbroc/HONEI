704b057fefadd649d5b56324a5b8cafc66b93767
Markus Geveler <apryde@gmx.de>
2007-11-25 17:41:58 +0000

    Added implementation of iterative refinement for sparse systems with Jacobi method and its test.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@978 0f804a50-a62f-0410-84e1-872c020a11f9

--- libmath/iterative_refinement_TEST.cc
+++ libmath/iterative_refinement_TEST.cc
@@ -495,10 +495,55 @@ class IterativeRefinementTestSparsePCGJAC:
 
         }
 };
+template <typename Tag_, typename DT1_>
+class IterativeRefinementTestSparseJAC:
+    public BaseTest
+{
+    public:
+        IterativeRefinementTestSparseJAC(const std::string & tag) :
+            BaseTest("Iterative Refinement with Jacobi solver test (sparse system)<" + tag + ">")
+        {
+        }
+
+        virtual void run() const
+        {
+            SparseMatrix<DT1_> A(3, 3);
+            DenseVector<DT1_> b(3, DT1_(1));
+            A[0][0] = DT1_(7);
+            A[0][1] = DT1_(-2);
+            A[0][2] = DT1_(0);
+            A[1][0] = DT1_(-2);
+            A[1][1] = DT1_(6);
+            A[1][2] = DT1_(2);
+            A[2][0] = DT1_(0);
+            A[2][1] = DT1_(2);
+            A[2][2] = DT1_(5);
+
+            b[0] = DT1_(3);
+            b[1] = DT1_(3);
+            b[2] = DT1_(0);
+
+            std::cout<<"A:"<<A<<endl;
+            std::cout<<"b:"<<b<<endl;
+            DenseVector<DT1_> result(IterativeRefinement<JAC, tags::CPU>::value(A,b,double(std::numeric_limits<double>::epsilon()), double(std::numeric_limits<double>::epsilon())));
+            DT1_ x_n = Norm< vnt_l_two, false, DT1_>::value(result);
+            DenseVector<DT1_> x_analytical(3, DT1_(0));
+            cout<<"RESULT(v1):"<<result<<endl;
+
+            x_analytical[0] = DT1_(2./3.);
+            x_analytical[1] = DT1_(5./6.);
+            x_analytical[2] = DT1_(-1./3.);
+            DT1_ x_analytical_n = Norm< vnt_l_two, false, DT1_>::value(x_analytical);
+            TEST_CHECK_EQUAL_WITHIN_EPS(x_analytical_n, x_n , double(0.001));
+
+        }
+};
+
 
 
 IterativeRefinementTestSparseCG<tags::CPU, double> iterref_test_double_sparseCG("double");
 IterativeRefinementTestSparsePCGJAC<tags::CPU, double> iterref_test_double_sparsePCGJAC("double");
+IterativeRefinementTestSparseJAC<tags::CPU, double> iterref_test_double_sparseJAC("double");
 
 IterativeRefinementTestDenseCG<tags::CPU, double> iterref_test_double_denseCG("double");
 IterativeRefinementTestDenseJAC<tags::CPU, double> iterref_test_double_denseJAC("double");
