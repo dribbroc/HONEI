420e073fa6b7d6ab8d3f0f3c7bb5dcb407197749
Markus Geveler <apryde@gmx.de>
2007-12-06 21:29:57 +0000

    Added implementation of bilinear interpolation in 2D and its test.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@1077 0f804a50-a62f-0410-84e1-872c020a11f9

---
+++ libmath/interpolation.hh
@@ -0,0 +1,92 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2007 Markus Geveler <apryde@gmx.de>
+ *
+ * This file is part of the MATH C++ library. LibMath is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibMath is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef LIBMATH_GUARD_INTERPOLATION_HH
+#define LIBMATH_GUARD_INTERPOLATION_HH 1
+
+#include <libla/dense_vector.hh>
+#include <libla/difference.hh>
+#include <libla/norm.hh>
+#include <algorithm>
+
+using namespace std;
+
+namespace honei
+{
+    namespace interpolation_methods
+    {
+        class LINEAR
+        {
+        };
+    }
+    /**
+     * \brief Interpolation in space with arbitrary dimensions.
+     *
+     * The referenced containers are invariant under this operation.
+     * In every case, a new object is created and returned.
+     *
+     */
+    template<typename Tag_, typename Method_>
+    struct Interpolation
+    {
+    };
+
+    template <typename Tag_>
+    struct Interpolation<Tag_, interpolation_methods::LINEAR>
+    {
+        public:
+            /**
+             * \brief Interpolation in space: 2D (bilinear).
+             * \param delta_x The stepsize in x direction.
+             * \param delta_y The stepsize in y direction.
+             * \param height The scalarfield representing the function.
+             * \param x The x value in parameter space.
+             * \param y The y value in parameter space.
+             */
+            template<typename ResPrec_>
+            static inline ResPrec_ value(const ResPrec_ delta_x, const ResPrec_ delta_y, const DenseMatrix<ResPrec_>& height, const ResPrec_ x, const ResPrec_ y)
+            {
+                ///Compute the lower left vertex of the cell, in which the vector (x y)^T falls in
+                ///parameter space:
+
+                ResPrec_ x_t(0);
+                ResPrec_ y_t(0);
+                while(x_t + delta_x < x)
+                {
+                    x_t += delta_x;
+                }
+                while(y_t + delta_y < y)
+                {
+                    y_t += delta_y;
+                }
+
+                unsigned long i(x_t/delta_x);
+                unsigned long j(y_t/delta_y);
+
+                ///Perform bilinear interpolation:
+                ResPrec_ result = height[i][j] * ( ResPrec_(1) - (y - y_t ))* (ResPrec_(1) - (x -x_t)) +
+                                  height[i+1][j] * (y - y_t ) * (ResPrec_(1) - (y - y_t) ) +
+                                  height[i][j+1] * (x -x_t) * (ResPrec_(1) - (y - y_t) ) +
+                                  height[i+1][j+1] * (y - y_t ) * (x -x_t);
+                return result;
+            }
+    };
+}
+
+#endif
---
+++ libmath/interpolation_TEST.cc
@@ -0,0 +1,55 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2007 Markus Geveler <apryde@gmx.de>
+ *
+ * This file is part of the Math C++ library. LibMath is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibMath is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <libmath/interpolation.hh>
+#include <unittest/unittest.hh>
+#include <libutil/stringify.hh>
+#include <iostream>
+
+using namespace honei;
+using namespace tests;
+using namespace std;
+using namespace interpolation_methods;
+template <typename Tag_, typename DT1_>
+class InterpolationTest:
+    public BaseTest
+{
+    public:
+        InterpolationTest(const std::string & tag) :
+            BaseTest("Interpolation test<" + tag + ">")
+        {
+            register_tag(Tag_::name);
+        }
+
+        virtual void run() const
+        {
+            DenseMatrix<DT1_> height(10,10, DT1_(0));
+            height[5][5] = DT1_(20);
+            height[6][5] = DT1_(20);
+            height[6][6] = DT1_(20);
+            height[5][6] = DT1_(20);
+
+            DT1_ delta_x(1);
+            DT1_ delta_y(1);
+            DT1_ result = Interpolation<Tag_, LINEAR>::value(delta_x, delta_y, height, DT1_(5.5), DT1_(5.5));
+            TEST_CHECK_EQUAL_WITHIN_EPS(DT1_(20), result, std::numeric_limits<DT1_>::epsilon());
+        }
+};
+InterpolationTest<tags::CPU, double> interpolation_test("double");
+
