11f400e034844e647361f60e238395262b28b551
Till Barz <till.barz@uni-dortmund.de>
2007-12-17 15:02:19 +0000

    Minor correction :).
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@1160 0f804a50-a62f-0410-84e1-872c020a11f9

--- cell/libla/dense_float_norm_l_two.cc
+++ cell/libla/dense_float_norm_l_two.cc
@@ -1,7 +1,6 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2007 Till Barz <till.barz@uni-dortmund.de>
  * Copyright (c) 2007 Sven Mallach <sven.mallach@honei.org>
  *
  * This file is part of the LA C++ library. LibLa is free software;
