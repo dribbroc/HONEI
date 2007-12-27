6ed69684d68d32126287291773a8c27b701a7351
Markus Geveler <apryde@gmx.de>
2007-12-27 23:17:32 +0000

    Added post processing module to libswe with GNUPLOT implementation and preparation for HDF5, generated file is deleted automatically by its test.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@1230 0f804a50-a62f-0410-84e1-872c020a11f9

---
+++ libswe/post_processing_TEST.cc
@@ -0,0 +1,50 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2007 Markus Geveler <apryde@gmx.de>
+ *
+ * This file is part of the Math C++ library. LibMath is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibMath is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <libswe/post_processing.hh>
+#include <unittest/unittest.hh>
+#include <libutil/stringify.hh>
+#include <iostream>
+
+using namespace tests;
+using namespace std;
+using namespace output_types;
+
+template <typename Tag_, typename DT1_>
+class PostProcessingTest:
+    public BaseTest
+{
+    public:
+        PostProcessingTest(const std::string & tag) :
+            BaseTest("PostProcessing test <" + tag + ">")
+        {
+            register_tag(Tag_::name);
+        }
+
+        virtual void run() const
+        {
+            DenseMatrix<DT1_> height(100, 100, DT1_(1.2345));
+            PostProcessing<output_types::GNUPLOT>::value(height, 1, 100, 100, 1);
+            remove("out1.dat");
+            TEST_CHECK(true);
+        }
+};
+PostProcessingTest<tags::CPU, float> source_test_float("float");
+PostProcessingTest<tags::CPU, double> source_test_double("double");
+
