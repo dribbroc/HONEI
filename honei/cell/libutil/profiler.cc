aa8aedd0a3501029edce1de4afba7bab0e3bfddd
Danny van Dyk <dyk@honei.org>
2008-01-20 22:46:12 +0000

    Oops. Forgot to add file.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@1380 0f804a50-a62f-0410-84e1-872c020a11f9

---
+++ honei/cell/libutil/profiler.cc
@@ -0,0 +1,26 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2008 Danny van Dyk <danny.dyk@uni-dortmund.de>
+ *
+ * This file is part of the Utility C++ library. LibUtil is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibUtil is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <honei/cell/libutil/profiler.hh>
+
+unsigned honei::cell::intern::profiler_top;
+
+honei::cell::LocalStoreAddress honei::cell::intern::profiler_addresses[16];
+
+unsigned honei::cell::intern::profiler_decrementers[16];
