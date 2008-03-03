8db2c9f05018dd0646f48af59979da0e116442b7
dyk <dyk@0f804a50-a62f-0410-84e1-872c020a11f9>
2008-03-03 01:44:28 +0000

    Added means to point aclocal to the m4 files on weird installs. Set environment variable ACLOCAL_INCLUDE accordingly.
    git-svn-id: file:///home/pg512/share/svn/pg512/trunk@1883 0f804a50-a62f-0410-84e1-872c020a11f9

--- autogen.bash
+++ autogen.bash
@@ -28,7 +28,7 @@ misc/do_m4.bash honei/libvisual/Makefile.am || exit $?
 run mkdir -p config
 run $(get libtoolize 1.5 ) --copy --force --automake
 rm -f config.cache
-run $(get aclocal 1.9 )
+run $(get aclocal 1.9 ) -I ${ACLOCAL_INCLUDE}
 run $(get autoheader 2.59 )
 run $(get autoconf 2.59 )
 run $(get automake 1.9 ) -a --copy
