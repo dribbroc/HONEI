0a09e7a42daf9782c54a18d21639a5b79231a971
Danny van Dyk <dyk@honei.org>
2008-04-18 14:11:25 +0200

    Added PrivateImplementationPattern
    Added class PrivateImplementationPattern that facilitates using the
    pattern of same name by just inheriting from this class template.
    
    There are two variants. The Shared pattern is to be used when the
    instances of a class shall be able to share one Implementation instance.
    The Single pattern is to be used when all instances of a class shall
    each have a dedicated instance of Implementation.
    
    Signed-off-by: Danny van Dyk <dyk@honei.org>

---
+++ honei/libutil/private_implementation_pattern-impl.hh
@@ -0,0 +1,57 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2008 Danny van Dyk <danny.dyk@uni-dortmund.de>
+ *
+ * This file is part of the Utility C++ library. LibUtil is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibUtil is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef LIBUTIL_GUARD_PRIVATE_IMPLEMENTATION_PATTERN_IMPL_HH
+#define LIBUTIL_GUARD_PRIVATE_IMPLEMENTATION_PATTERN_IMPL_HH 1
+
+#include <honei/libutil/private_implementation_pattern.hh>
+
+namespace honei
+{
+    template <typename T_>
+    PrivateImplementationPattern<T_, Shared>::PrivateImplementationPattern(Implementation<T_> * imp) :
+        _imp(imp)
+    {
+    }
+
+    template <typename T_>
+    PrivateImplementationPattern<T_, Shared>::PrivateImplementationPattern(std::tr1::shared_ptr<Implementation<T_> > imp) :
+        _imp(imp)
+    {
+    }
+
+    template <typename T_>
+    PrivateImplementationPattern<T_, Shared>::~PrivateImplementationPattern()
+    {
+    }
+
+    template <typename T_>
+    PrivateImplementationPattern<T_, Single>::PrivateImplementationPattern(Implementation<T_> * imp) :
+        _imp(imp)
+    {
+    }
+
+    template <typename T_>
+    PrivateImplementationPattern<T_, Single>::~PrivateImplementationPattern()
+    {
+        delete _imp;
+    }
+}
+
+#endif
---
+++ honei/libutil/private_implementation_pattern.hh
@@ -0,0 +1,90 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2008 Danny van Dyk <danny.dyk@uni-dortmund.de>
+ *
+ * This file is part of the Utility C++ library. LibUtil is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibUtil is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef LIBUTIL_GUARD_PRIVATE_IMPLEMENTATION_PATTERN_HH
+#define LIBUTIL_GUARD_PRIVATE_IMPLEMENTATION_PATTERN_HH 1
+
+#include <tr1/memory>
+
+namespace honei
+{
+    /**
+     * \{
+     * \name Private implementation pattern storage tags
+     *
+     * Possible tags that govern the storage of private implementations.
+     */
+
+    /**
+     * Tag for a shared private implementation.
+     */
+    struct Shared;
+
+    /**
+     * Tag for a single private implementation.
+     */
+    struct Single;
+
+    /// \}
+
+    /**
+     * Private implementation data, to be specialised for any class that uses
+     * PrivateImplementationPattern.
+     */
+    template <typename T_> struct Implementation;
+
+    /**
+     * \{
+     *
+     * PrivateImplementationPattern is a utility base class that can be used to
+     * easily create classes with private implementation.
+     */
+
+    template <typename T_, typename Method_> class PrivateImplementationPattern;
+
+    template <typename T_> class PrivateImplementationPattern<T_, Shared>
+    {
+        protected:
+            /// Our implementation.
+            std::tr1::shared_ptr<Implementation<T_> > _imp;
+
+        public:
+            explicit PrivateImplementationPattern(Implementation<T_> * imp);
+
+            explicit PrivateImplementationPattern(std::tr1::shared_ptr<Implementation<T_> > imp);
+
+            ~PrivateImplementationPattern();
+    };
+
+    template <typename T_> class PrivateImplementationPattern<T_, Single>
+    {
+        protected:
+            /// Our implementation.
+            Implementation<T_> * _imp;
+
+        public:
+            explicit PrivateImplementationPattern(Implementation<T_> * imp);
+
+            ~PrivateImplementationPattern();
+    };
+
+    /// \}
+}
+
+#endif
