18b416ceb5adf234e51444b41329e04d92c18551
Markus <markus.geveler@tu-dortmund.de>
2008-04-21 14:59:10 +0200

    Added constructor to solver, added test
    Signed-off-by: Markus <markus.geveler@tu-dortmund.de>

--- honei/liblbm/solver_labswe.hh
+++ honei/liblbm/solver_labswe.hh
@@ -28,14 +28,77 @@
  * \ingroup grpliblbm
  **/
 
+#include <honei/libla/dense_vector.hh>
+#include <honei/libla/dense_matrix.hh>
+
 namespace honei
 {
+
+	namespace lbm_lattice_types
+	{
+		class D2Q9;
+	}
+	namespace lbm_grid_types
+	{
+		class RECTANGULAR;
+	}
+	namespace lbm_source_types
+	{
+		class SIMPLE;
+	}
+	namespace lbm_source_shemes
+	{
+		class BASIC;
+		class CENTERED;
+	}
+	namespace lbm_boundary_types
+	{
+		class NOSLIP_PERIODIC;
+	}
+
 	template<typename Tag_,
 			 typename ResPrec_,
 			 typename SourceType_,
 			 typename SourceSheme_,
+			 typename GridType_,
+			 typename LatticeType_,
 			 typename BoundaryType_>
 	class SolverLABSWE
 	{
-	}
+	};
+
+	template<typename Tag_, typename ResPrec_>
+	class SolverLABSWE<Tag_, ResPrec_, lbm_source_types::SIMPLE, lbm_source_shemes::BASIC, lbm_grid_types::RECTANGULAR, lbm_lattice_types::D2Q9, lbm_boundary_types::NOSLIP_PERIODIC>
+	{
+		private:
+			/** Global variables.
+			  *
+			 **/
+			ResPrec_ _relaxation_time, _delta_x, _delta_y, _delta_t;
+			DenseMatrix<ResPrec_>* _height;
+			DenseMatrix<ResPrec_>* _u;
+			DenseMatrix<ResPrec_>* _v;
+
+			unsigned long _grid_width, _grid_height;
+
+			DenseVector<ResPrec_>* _equilibrium_distribution;
+			DenseVector<ResPrec_>* _source_x;
+			DenseVector<ResPrec_>* _source_y;
+
+			/** Global constants.
+			  *
+			 **/
+			ResPrec_ _n_alpha, _e, _gravity;
+			DenseVector<ResPrec_>* _distribution_vector_x;
+			DenseVector<ResPrec_>* _distribution_vector_y;
+
+
+		public:
+			SolverLABSWE(ResPrec_ dx, ResPrec_ dy, ResPrec_ dt, unsigned long gx, unsigned long gy)
+			{
+
+			}
+	};
+
 }
+#endif
