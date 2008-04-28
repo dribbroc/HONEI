44a309ab4762c1ca0048ddaf71c58439fcb05b20
Dirk Ribbrock <dribbrock@uni-dortmund.de>
2008-04-28 11:21:13 +0200

    Refurbished test output, by inserting white spaces.
    Signed-off-by: Dirk Ribbrock <dribbrock@uni-dortmund.de>

--- unittest/run.sh
+++ unittest/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # vim: set ft=sh sw=4 sts=4 et :
 
-echo ">>> test ${1}[BACKENDS=${BACKENDS},TYPE=${TYPE}]"
+echo ">>> test ${1} [BACKENDS=${BACKENDS},TYPE=${TYPE}]"
 ${@} ${TYPE} ${BACKENDS}
