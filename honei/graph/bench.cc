5e35848c97f0cd6274dcffc7a640d7c60fb3afea
Dirk Ribbrock <dribbrock@uni-dortmund.de>
2008-04-29 14:12:51 +0200

    Removed deprecated benchmark files
    Signed-off-by: Dirk Ribbrock <dribbrock@uni-dortmund.de>

--- honei/graph/bench.cc
+++
@@ -1,9 +0,0 @@
-#define ALLBENCH
-#include <benchmark/benchmark.cc> 
-#include <string>
-#include <tr1/memory>
-
-#include <honei/graph/breadth_first_search_BENCHMARK.cc>
-#include <honei/graph/position_BENCHMARK.cc>
-#include <honei/graph/node_distance_BENCHMARK.cc>
-#include <honei/graph/graph_BENCHMARK.cc>
