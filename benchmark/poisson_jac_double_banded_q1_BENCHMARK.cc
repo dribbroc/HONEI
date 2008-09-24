40f697a9d21fc1d6fdd210474395791bbd5424b2
Markus Geveler <markus.geveler@tu-dortmund.de>
2008-09-24 22:38:18 +0200

    Added double benchmarks for JAC SMoother variants
    Signed-off-by: Markus Geveler <markus.geveler@tu-dortmund.de>

---
+++ benchmark/poisson_jac_double_banded_q1_BENCHMARK.cc
@@ -0,0 +1,308 @@
+/* vim: set number sw=4 sts=4 et nofoldenable : */
+
+/*
+ * Copyright (c) 2008 Markus Geveler <apryde@gmx.de>
+ *
+ * This file is part of the Math C++ library. Math is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * Math is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef ALLBENCH
+#include <benchmark/benchmark.cc>
+#include <tr1/memory>
+#include <string>
+#endif
+
+#include <honei/math/jacobi.hh>
+#include <honei/math/endian_swap.hh>
+
+using namespace std;
+using namespace honei;
+
+template <typename Tag_, typename DataType_>
+
+class PoissonJACBenchQ1_1 :
+    public Benchmark
+{
+    private:
+        unsigned long _count;
+    public:
+        PoissonJACBenchQ1_1(const std::string & id, unsigned long count) :
+            Benchmark(id)
+        {
+            register_tag(Tag_::name);
+            _count = count;
+        }
+
+        virtual void run()
+        {
+            int n;
+
+            FILE* file;
+
+            double* dd;
+
+            double* ll;
+            double* ld;
+            double* lu;
+            double* dl;
+            double* du;
+            double* ul;
+            double* ud;
+            double* uu;
+
+            double* b;
+            double* ana_sol;
+            double* ref_sol;
+
+            file = fopen("../honei/math/testdata/4225/ehq.1.1.1.1.bin", "rb");
+            fread(&n, sizeof(int), 1, file);
+
+#ifdef HONEI_CELL
+            unsigned char b1, b2, b3, b4;
+            b1 = n & 255;
+            b2 = ( n >> 8 ) & 255;
+            b3 = ( n>>16 ) & 255;
+            b4 = ( n>>24 ) & 255;
+            n = ((int)b1 << 24) + ((int)b2 << 16) + ((int)b3 << 8) + b4;
+#endif
+            dd = new double[n];
+            ll = new double[n];
+            ld = new double[n];
+            lu = new double[n];
+            dl = new double[n];
+            du = new double[n];
+            ul = new double[n];
+            ud = new double[n];
+            uu = new double[n];
+            b = new double[n];
+            ana_sol = new double[n];
+            ref_sol = new double[n];
+
+            fread(dd, sizeof(double), n, file);
+            fread(ll, sizeof(double), n, file);
+            fread(ld, sizeof(double), n, file);
+            fread(lu, sizeof(double), n, file);
+            fread(dl, sizeof(double), n, file);
+            fread(du, sizeof(double), n, file);
+            fread(ul, sizeof(double), n, file);
+            fread(ud, sizeof(double), n, file);
+            fread(uu, sizeof(double), n, file);
+            fread(b,  sizeof(double), n, file);
+            fread(ana_sol, sizeof(double), n, file);
+            fread(ref_sol, sizeof(double), n, file);
+            fclose(file);
+
+#ifdef HONEI_CELL
+            for(unsigned long i(0); i < n; ++i)
+            {
+                dd[i] = DoubleSwap(dd[i]);
+                ll[i] = DoubleSwap(ll[i]);
+                ld[i] = DoubleSwap(ld[i]);
+                lu[i] = DoubleSwap(lu[i]);
+                dl[i] = DoubleSwap(dl[i]);
+                du[i] = DoubleSwap(du[i]);
+                ul[i] = DoubleSwap(ul[i]);
+                ud[i] = DoubleSwap(ud[i]);
+                uu[i] = DoubleSwap(uu[i]);
+                b[i] = DoubleSwap(b[i]);
+                ana_sol[i] = DoubleSwap(ana_sol[i]);
+                ref_sol[i] = DoubleSwap(ref_sol[i]);
+
+            }
+#endif
+            DenseVector<double> dd_v(n, double(0));
+            DenseVector<double> ll_v(n, double(0));
+            DenseVector<double> ld_v(n, double(0));
+            DenseVector<double> lu_v(n, double(0));
+            DenseVector<double> dl_v(n, double(0));
+            DenseVector<double> du_v(n, double(0));
+            DenseVector<double> ul_v(n, double(0));
+            DenseVector<double> ud_v(n, double(0));
+            DenseVector<double> uu_v(n, double(0));
+            DenseVector<double> b_v(n, double(0));
+            DenseVector<double> ana_sol_v(n, double(0));
+            DenseVector<double> ref_sol_v(n, double(0));
+            for(unsigned long i = 0; i < n; ++i)
+            {
+                dd_v[i] = dd[i];
+                ll_v[i] = ll[i];
+                ld_v[i] = ld[i];
+                lu_v[i] = lu[i];
+                dl_v[i] = dl[i];
+                du_v[i] = du[i];
+                ul_v[i] = ul[i];
+                ud_v[i] = ud[i];
+                uu_v[i] = uu[i];
+                b_v[i] = b[i];
+                ana_sol_v[i] = ana_sol[i];
+                ref_sol_v[i] = ref_sol[i];
+            }
+
+            DenseVector<double> to_smooth(n, double(0));
+            BandedMatrixQ1<double> A(n , ll_v, ld_v, lu_v, dl_v, dd_v, du_v, ul_v, ud_v, uu_v);
+
+            double omega(0.7);
+            for(unsigned long i(0) ; i < _count ; ++i)
+            {
+                BENCHMARK(Jacobi<Tag_>::value(A, b_v, omega));
+            }
+            evaluate();
+        }
+};
+PoissonJACBenchQ1_1<tags::CPU, double> poisson_jac_bench_float("Poisson JAC Smoother 1 benchmark double CPU", 100);
+#ifdef HONEI_SSE
+PoissonJACBenchQ1_1<tags::CPU::SSE, double> poisson_jac_bench_float_sse("Poisson JAC Smoother 1 benchmark double SSE", 100);
+#endif
+#ifdef HONEI_CELL
+PoissonJACBenchQ1_1<tags::Cell, double> poisson_jac_bench_float_cell("Poisson JAC Smoother 1 benchmark double Cell", 100);
+#endif
+
+template <typename Tag_, typename DataType_>
+
+class PoissonJACBenchQ1_2 :
+    public Benchmark
+{
+    private:
+        unsigned long _count;
+    public:
+        PoissonJACBenchQ1_2(const std::string & id, unsigned long count) :
+            Benchmark(id)
+        {
+            register_tag(Tag_::name);
+            _count = count;
+        }
+
+        virtual void run()
+        {
+            int n;
+
+            FILE* file;
+
+            double* dd;
+
+            double* ll;
+            double* ld;
+            double* lu;
+            double* dl;
+            double* du;
+            double* ul;
+            double* ud;
+            double* uu;
+
+            double* b;
+            double* ana_sol;
+            double* ref_sol;
+
+            file = fopen("../honei/math/testdata/4225/ehq.1.1.1.1.bin", "rb");
+            fread(&n, sizeof(int), 1, file);
+
+#ifdef HONEI_CELL
+            unsigned char b1, b2, b3, b4;
+            b1 = n & 255;
+            b2 = ( n >> 8 ) & 255;
+            b3 = ( n>>16 ) & 255;
+            b4 = ( n>>24 ) & 255;
+            n = ((int)b1 << 24) + ((int)b2 << 16) + ((int)b3 << 8) + b4;
+#endif
+            dd = new double[n];
+            ll = new double[n];
+            ld = new double[n];
+            lu = new double[n];
+            dl = new double[n];
+            du = new double[n];
+            ul = new double[n];
+            ud = new double[n];
+            uu = new double[n];
+            b = new double[n];
+            ana_sol = new double[n];
+            ref_sol = new double[n];
+
+            fread(dd, sizeof(double), n, file);
+            fread(ll, sizeof(double), n, file);
+            fread(ld, sizeof(double), n, file);
+            fread(lu, sizeof(double), n, file);
+            fread(dl, sizeof(double), n, file);
+            fread(du, sizeof(double), n, file);
+            fread(ul, sizeof(double), n, file);
+            fread(ud, sizeof(double), n, file);
+            fread(uu, sizeof(double), n, file);
+            fread(b,  sizeof(double), n, file);
+            fread(ana_sol, sizeof(double), n, file);
+            fread(ref_sol, sizeof(double), n, file);
+            fclose(file);
+
+#ifdef HONEI_CELL
+            for(unsigned long i(0); i < n; ++i)
+            {
+                dd[i] = DoubleSwap(dd[i]);
+                ll[i] = DoubleSwap(ll[i]);
+                ld[i] = DoubleSwap(ld[i]);
+                lu[i] = DoubleSwap(lu[i]);
+                dl[i] = DoubleSwap(dl[i]);
+                du[i] = DoubleSwap(du[i]);
+                ul[i] = DoubleSwap(ul[i]);
+                ud[i] = DoubleSwap(ud[i]);
+                uu[i] = DoubleSwap(uu[i]);
+                b[i] = DoubleSwap(b[i]);
+                ana_sol[i] = DoubleSwap(ana_sol[i]);
+                ref_sol[i] = DoubleSwap(ref_sol[i]);
+
+            }
+#endif
+            DenseVector<double> dd_v(n, double(0));
+            DenseVector<double> ll_v(n, double(0));
+            DenseVector<double> ld_v(n, double(0));
+            DenseVector<double> lu_v(n, double(0));
+            DenseVector<double> dl_v(n, double(0));
+            DenseVector<double> du_v(n, double(0));
+            DenseVector<double> ul_v(n, double(0));
+            DenseVector<double> ud_v(n, double(0));
+            DenseVector<double> uu_v(n, double(0));
+            DenseVector<double> b_v(n, double(0));
+            DenseVector<double> ana_sol_v(n, double(0));
+            DenseVector<double> ref_sol_v(n, double(0));
+            for(unsigned long i = 0; i < n; ++i)
+            {
+                dd_v[i] = dd[i];
+                ll_v[i] = ll[i];
+                ld_v[i] = ld[i];
+                lu_v[i] = lu[i];
+                dl_v[i] = dl[i];
+                du_v[i] = du[i];
+                ul_v[i] = ul[i];
+                ud_v[i] = ud[i];
+                uu_v[i] = uu[i];
+                b_v[i] = b[i];
+                ana_sol_v[i] = ana_sol[i];
+                ref_sol_v[i] = ref_sol[i];
+            }
+
+            DenseVector<double> to_smooth(n, double(0));
+            BandedMatrixQ1<double> A(n , ll_v, ld_v, lu_v, dl_v, dd_v, du_v, ul_v, ud_v, uu_v);
+
+            double omega(0.7);
+            for(unsigned long i(0) ; i < _count ; ++i)
+            {
+                BENCHMARK(Jacobi<Tag_>::value(to_smooth, A, b_v, 4,  omega));
+            }
+            evaluate();
+        }
+};
+PoissonJACBenchQ1_2<tags::CPU, double> poisson_jac_bench_float_2("Poisson JAC Smoother 2 benchmark double CPU", 100);
+#ifdef HONEI_SSE
+PoissonJACBenchQ1_2<tags::CPU::SSE, double> poisson_jac_bench_float_sse_2("Poisson JAC Smoother 2 benchmark double SSE", 100);
+#endif
+#ifdef HONEI_CELL
+PoissonJACBenchQ1_2<tags::Cell, double> poisson_jac_bench_float_cell_2("Poisson JAC Smoother 2 benchmark double Cell", 100);
+#endif
