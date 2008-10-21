d2525bd8e8cd19c8e0e59bcbe9e74af90b99d513
Dirk Ribbrock <dirk.ribbrock@uni-dortmund.de>
2008-10-21 14:29:34 +0200

    Fixed wrong segment end bug in direction packing
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@uni-dortmund.de>

--- honei/lbm/grid_packer.hh
+++ honei/lbm/grid_packer.hh
@@ -67,7 +67,7 @@ namespace honei
                             ++end;
                         }
                         dir_index.push_back(limits.at(end) + 1);
-                        i = end - 1;
+                        i = end;
                     }
                 }
             }
