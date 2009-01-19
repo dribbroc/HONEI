a689127023e31d29a30c2074cd523cf4ef543e05
Dirk Ribbrock <dirk.ribbrock@uni-dortmund.de>
2009-01-19 15:54:26 +0100

    Added EquilibriumDistributionGrid benchmark
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@uni-dortmund.de>

--- benchmark/equilibrium_distribution_grid_BENCHMARK.cc
+++ benchmark/equilibrium_distribution_grid_BENCHMARK.cc
@@ -1,13 +1,13 @@
 /* vim: set number sw=4 sts=4 et nofoldenable : */
 
 /*
- * Copyright (c) 2008 Markus Geveler <apryde@gmx.de>
+ * Copyright (c) 2009 Dirk Ribbrock <dirk.ribbrock@uni-dortmund.de>
  *
- * This file is part of the SWE C++ library. LiSWE is free software;
+ * This file is part of the HONEI C++ library. HONEI is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
  * Public License version 2, as published by the Free Software Foundation.
  *
- * LibSWE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * HONEI is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
@@ -16,40 +16,46 @@
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
+#ifndef ALLBENCH
+#include <benchmark/benchmark.cc>
+#include <tr1/memory>
+#include <string>
+#endif
+
+#include <honei/lbm/equilibrium_distribution_grid.hh>
+#include <honei/lbm/grid_packer.hh>
 #include <honei/lbm/solver_labswe_grid.hh>
-#include <honei/lbm/partial_derivative.hh>
-#include <honei/swe/post_processing.hh>
-#include <unittest/unittest.hh>
-#include <iostream>
 #include <honei/swe/volume.hh>
 #include <honei/math/quadrature.hh>
-#include <honei/lbm/grid.hh>
-#include <honei/lbm/grid_packer.hh>
+#include <honei/backends/cuda/operations.hh>
+#include <honei/lbm/partial_derivative.hh>
 
-using namespace honei;
-using namespace tests;
 using namespace std;
-using namespace output_types;
-using namespace lbm::lbm_lattice_types;
+using namespace honei;
+using namespace lbm;
+using namespace lbm_lattice_types;
 
-//#define SOLVER_VERBOSE
-//#define SOLVER_POSTPROCESSING
-
-template <typename Tag_, typename DataType_>
-class SolverLABSWEGridTest :
-    public TaggedTest<Tag_>
+template <typename Tag_, typename DataType_, typename Dir_>
+class EquilibriumDistributionGridBench :
+    public Benchmark
 {
+    private:
+        unsigned long _size;
+        int _count;
     public:
-        SolverLABSWEGridTest(const std::string & type) :
-            TaggedTest<Tag_>("solver_labswe_grid_test<" + type + ">")
+        EquilibriumDistributionGridBench(const std::string & id, unsigned long size, int count) :
+            Benchmark(id)
         {
+            register_tag(Tag_::name);
+            _size = size;
+            _count = count;
         }
 
-        virtual void run() const
+        virtual void run()
         {
-            unsigned long g_h(50);
-            unsigned long g_w(50);
-            unsigned long timesteps(100);
+            unsigned long g_h(_size);
+            unsigned long g_w(_size);
 
             DenseMatrix<DataType_> h(g_h, g_w, DataType_(0.05));
             Cylinder<DataType_> c1(h, DataType_(0.02), 25, 25);
@@ -84,29 +90,22 @@ class SolverLABSWEGridTest :
 
             solver.do_preprocessing();
 
-            for(unsigned long i(0); i < timesteps; ++i)
+            DataType_ g(9.81);
+            DataType_ e(1.);
+
+            for(int i = 0; i < _count; ++i)
             {
-#ifdef SOLVER_VERBOSE
-                std::cout<<"Timestep: " << i << "/" << timesteps << std::endl;
-#endif
-                solver.solve();
-#ifdef SOLVER_POSTPROCESSING
-                GridPacker<D2Q9, NOSLIP, DataType_>::unpack(grid, info, data);
-                PostProcessing<GNUPLOT>::value(*grid.h, 1, g_w, g_h, i);
+                BENCHMARK((EquilibriumDistributionGrid<Tag_, lbm_applications::LABSWE>::value(g, e, info, data)));
+#ifdef HONEI_CUDA
+                        cuda_thread_synchronize();
 #endif
             }
-#ifdef SOLVER_VERBOSE
-            GridPacker<D2Q9, NOSLIP, DataType_>::unpack(grid, info, data);
-            std::cout << *grid.h << std::endl;
-#endif
-            TEST_CHECK(true);
+            evaluate();
         }
-
 };
-SolverLABSWEGridTest<tags::CPU, float> solver_test_float("float");
-SolverLABSWEGridTest<tags::CPU, double> solver_test_double("double");
-SolverLABSWEGridTest<tags::CPU::MultiCore, float> mc_solver_test_float("float");
-SolverLABSWEGridTest<tags::CPU::MultiCore, double> mc_solver_test_double("double");
+
+EquilibriumDistributionGridBench<tags::CPU, float, D2Q9::DIR_0> eq_dist_grid_bench_float("EquilibriumDistributionGridBenchmark - size: 1000, float", 1000, 10);
+EquilibriumDistributionGridBench<tags::CPU, double, D2Q9::DIR_0> eq_dist_grid_bench_double("EquilibriumDistributionGrid Benchmark - size: 1000, double", 1000, 10);
 #ifdef HONEI_CUDA
-SolverLABSWEGridTest<tags::GPU::CUDA, float> cuda_solver_test_float("float");
+EquilibriumDistributionGridBench<tags::GPU::CUDA, float, D2Q9::DIR_0> cuda_eq_dist_grid_bench_float("CUDA EquilibriumDistributionGridBenchmark - size: 1000, float", 1000, 10);
 #endif
