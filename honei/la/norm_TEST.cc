1926bd6dbc84c68b240c10445952f676a1fbbe37
Dirk Ribbrock <dirk.ribbrock@uni-dortmund.de>
2009-05-08 10:49:39 +0200

    Fixed norm cell test
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@uni-dortmund.de>

--- honei/la/norm_TEST.cc
+++ honei/la/norm_TEST.cc
@@ -246,11 +246,11 @@ class DenseVectorRangeNormQuickTest :
 
                 DataType_ vmax(Norm<vnt_max, false, Tag_>::value(dv));
                 DataType_ smax(s / 1.23456789);
-                TEST_CHECK_EQUAL_WITHIN_EPS(vmax, smax, std::numeric_limits<float>::epsilon());
+                TEST_CHECK_EQUAL_WITHIN_EPS(vmax, smax, std::numeric_limits<DataType_>::epsilon());
 
                 DataType_ v1(Norm<vnt_l_one, false, Tag_>::value(dv));
                 DataType_ s1(s * (s + 1) / 2 / 1.23456789);
-                DataType_ eps1(s1 * 10 * std::numeric_limits<float>::epsilon());
+                DataType_ eps1(s1 * 15 * std::numeric_limits<float>::epsilon());
                 TEST_CHECK_EQUAL_WITHIN_EPS(v1, s1, eps1);
 
                 DataType_ v2(Norm<vnt_l_two, false, Tag_>::value(dv));
