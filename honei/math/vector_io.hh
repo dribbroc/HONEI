a32fef75c4634c83b7b91601aa00395d97642ecc
Markus Geveler <markus.geveler@tu-dortmund.de>
2009-06-09 23:48:02 +0200

    Added vector_io functions, added test-files
    Signed-off-by: Markus Geveler <markus.geveler@tu-dortmund.de>

---
+++ honei/math/vector_io.hh
@@ -0,0 +1,117 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2009 Markus Geveler <apryde@gmx.de>
+ *
+ * This file is part of the MATH C++ library. LibMath is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibMath is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef MATH_GUARD_VECTOR_IO_HH
+#define MATH_GUARD_VECTOR_IO_HH 1
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <honei/la/dense_vector.hh>
+
+using namespace honei;
+
+namespace io_formats
+{
+    class EXP;
+}
+
+
+template<typename IOFormat_>
+class VectorIO
+{
+};
+
+template<>
+class VectorIO<io_formats::EXP>
+{
+    public:
+        static void get_size(std::string filename, unsigned long & n_z, unsigned long & data_begin)
+        {
+            n_z = 0;
+            data_begin = 0;
+
+            std::ifstream file(filename.c_str());
+            if(file.is_open())
+            {
+                while(!file.eof())
+                {
+                    std::string line;
+                    std::getline(file, line);
+
+                    if(line.find("#", 0) < line.npos)
+                    {
+                        ++data_begin;
+                        continue;
+                    }
+                    else
+                    {
+                        ++n_z;
+                    }
+
+                }
+
+                file.close();
+            }
+            else
+                throw honei::InternalError("Unable to open Vector file.");
+        }
+
+        template<typename DT_>
+            static void read_vector(std::string filename, DenseVector<DT_> & data)
+            {
+                unsigned long non_data_lines, non_zeros;
+                get_size(filename, non_zeros, non_data_lines);
+
+                std::ifstream file(filename.c_str());
+                file.is_open();
+
+                for(unsigned long i(0) ; i < non_data_lines ; ++i)
+                {
+                    std::string line;
+                    std::getline(file, line);
+                }
+
+                for(unsigned long j(0) ; j < non_zeros ; ++j)
+                {
+                    std::string line;
+                    std::getline(file, line);
+
+                    std::string n_z_s;
+
+                    std::string::size_type first_digit(line.find_first_not_of(" "));
+                    line.erase(0, first_digit);
+                    std::string::size_type eol(line.length());
+                    for(unsigned long i(0) ; i < eol ; ++i)
+                    {
+                        n_z_s.append(1, line[i]);
+                    }
+
+                    DT_ n_z = (DT_)atof(n_z_s.c_str());
+
+                    data[j] = n_z;
+
+                }
+                file.close();
+
+            }
+};
+
+#endif
---
+++ honei/math/vector_io_TEST.cc
@@ -0,0 +1,57 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2009 Markus Geveler <apryde@gmx.de>
+ *
+ * This file is part of the Math C++ library. LibMath is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibMath is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <honei/math/vector_io.hh>
+#include <unittest/unittest.hh>
+#include <honei/util/stringify.hh>
+#include <honei/la/product.hh>
+#include <iostream>
+
+using namespace honei;
+using namespace tests;
+using namespace std;
+
+template<typename DT_, typename Tag_>
+class VectorIOTest:
+    public BaseTest
+{
+    public:
+        VectorIOTest(const std::string & tag) :
+            BaseTest("EXP vector Format I/O test " + tag)
+        {
+            register_tag(Tag_::name);
+        }
+
+        virtual void run() const
+        {
+            std::string filename(HONEI_SOURCEDIR);
+            filename += "/honei/math/testdata/area51_rhs_0";
+            unsigned long non_zeros, non_data;
+
+            VectorIO<io_formats::EXP>::get_size(filename, non_zeros, non_data);
+            DenseVector<DT_> data(non_zeros);
+
+            VectorIO<io_formats::EXP>::read_vector(filename, data);
+
+            std::cout << data << std::endl;
+        }
+};
+VectorIOTest<float, tags::CPU> vecio_test_float_dense("float");
+VectorIOTest<double, tags::CPU> vecio_test_double_dense("double");
+
