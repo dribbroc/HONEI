31b988e20edd8f38b8f1048090957c0f72c49050
Markus Geveler <markus.geveler@tu-dortmund.de>
2009-06-26 15:49:15 +0200

    Added solver lbm fsi version and test
    Signed-off-by: Markus Geveler <markus.geveler@tu-dortmund.de>

---
+++ honei/lbm/solver_lbm_fsi_TEST.cc
@@ -0,0 +1,101 @@
+/* vim: set number sw=4 sts=4 et nofoldenable : */
+
+/*
+ * Copyright (c) 2009 Markus Geveler <apryde@gmx.de>
+ *
+ * This file is part of the SWE C++ library. LiSWE is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibSWE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <honei/lbm/solver_lbm_fsi.hh>
+#include <honei/lbm/partial_derivative.hh>
+#include <honei/swe/post_processing.hh>
+#include <unittest/unittest.hh>
+#include <iostream>
+#include <honei/swe/volume.hh>
+#include <honei/math/quadrature.hh>
+#include <honei/lbm/grid.hh>
+#include <honei/lbm/grid_packer.hh>
+#include <honei/lbm/scenario_collection.hh>
+
+using namespace honei;
+using namespace tests;
+using namespace std;
+using namespace output_types;
+using namespace lbm::lbm_lattice_types;
+
+#define SOLVER_VERBOSE
+#define SOLVER_POSTPROCESSING
+
+template <typename Tag_, typename DataType_>
+class SolverLBMFSITest :
+    public TaggedTest<Tag_>
+{
+    public:
+        SolverLBMFSITest(const std::string & type) :
+            TaggedTest<Tag_>("solver_lbm_fsi_test<" + type + ">")
+        {
+        }
+
+        virtual void run() const
+        {
+            unsigned long g_h(50);
+            unsigned long g_w(50);
+            unsigned long timesteps(100);
+
+
+            Grid<D2Q9, DataType_> grid;
+
+            ScenarioCollection::get_scenario(0, g_h, g_w, grid);
+
+            PackedGridData<D2Q9, DataType_>  data;
+            PackedSolidData<D2Q9, DataType_>  solids;
+            PackedGridInfo<D2Q9> info;
+
+            DenseMatrix<bool> line(g_h, g_w, false);
+            DenseMatrix<bool> bound(g_h, g_w, false);
+            DenseMatrix<bool> stf(g_h, g_w, false);
+            DenseMatrix<bool> sol(g_h, g_w, false);
+
+            GridPacker<D2Q9, NOSLIP, DataType_>::pack(grid, info, data);
+            GridPacker<D2Q9, lbm_boundary_types::NOSLIP, DataType_>::cuda_pack(info, data);
+            GridPackerFSI<D2Q9, NOSLIP, DataType_>::allocate(data, solids);
+            GridPackerFSI<D2Q9, NOSLIP, DataType_>::pack(grid, data, solids, line, bound, stf, sol, *grid.obstacles);
+
+            SolverLBMFSI<Tag_, lbm_applications::LABSWE, DataType_,lbm_force::CENTRED, lbm_source_schemes::BED_FULL, lbm_grid_types::RECTANGULAR, lbm_lattice_types::D2Q9, lbm_boundary_types::NOSLIP, lbm_modes::DRY> solver(&info, &data, &solids, grid.d_x, grid.d_y, grid.d_t, grid.tau);
+
+            solver.do_preprocessing();
+            std::cout << "Solving: " << grid.description << std::endl;
+            for(unsigned long i(0); i < timesteps; ++i)
+            {
+#ifdef SOLVER_VERBOSE
+                std::cout<<"Timestep: " << i << "/" << timesteps << std::endl;
+#endif
+                solver.solve();
+#ifdef SOLVER_POSTPROCESSING
+                solver.do_postprocessing();
+                GridPacker<D2Q9, NOSLIP, DataType_>::unpack(grid, info, data);
+                PostProcessing<GNUPLOT>::value(*grid.h, 1, g_w, g_h, i);
+#endif
+            }
+            solver.do_postprocessing();
+            GridPacker<D2Q9, NOSLIP, DataType_>::unpack(grid, info, data);
+#ifdef SOLVER_VERBOSE
+            std::cout << *grid.h << std::endl;
+#endif
+            for (unsigned long i(0) ; i < (*grid.h).rows() ; ++i)
+                for(unsigned long j(0) ; j < (*grid.h).columns() ; ++j)
+                    TEST_CHECK_EQUAL_WITHIN_EPS((*grid.h)( i , j), DataType_(0.02), DataType_(0.1));
+        }
+
+};
+SolverLBMFSITest<tags::CPU, float> solver_test_float("float");
