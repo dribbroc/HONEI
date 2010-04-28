677329b9be6bc137a1d9f0c843b2d492d2d175e3
mgeveler <markus.geveler@math.tu-dortmund.de>
2010-04-28 16:02:54 +0200

    Fixed client
    Signed-off-by: mgeveler <markus.geveler@math.tu-dortmund.de>

--- clients/poisson/scenario_controller.hh
+++ clients/poisson/scenario_controller.hh
@@ -227,7 +227,7 @@ template<typename Tag_, typename Prec_> class ScenarioController :
                         }
 
                         DenseVector<Prec_> result(n, Prec_(0));
-                        Multigrid<Tag_, Tag_, JAC, CYCLE::V, FIXED >::value(info.a[info.max_level], info.rhs[info.max_level], result, (unsigned long)11, std::numeric_limits<Prec_>::epsilon(), info);
+                        Multigrid<Tag_, Tag_, NONE, JAC, CYCLE::V, FIXED >::value(info.a[info.max_level], info.rhs[info.max_level], result, (unsigned long)11, std::numeric_limits<Prec_>::epsilon(), info);
 
                         //write result to scalarfield:
                         unsigned long row_index(0);
@@ -419,9 +419,9 @@ template<typename Tag_, typename Prec_> class ScenarioController :
                         DenseVector<Prec_> result(n, Prec_(0));
 
 #ifdef HONEI_SSE
-                        Multigrid<tags::CPU::SSE, tags::CPU::SSE, JAC, CYCLE::V, MIXED>::value(A, RHS, result, (unsigned long)11, std::numeric_limits<double>::epsilon(), info);
+                        Multigrid<tags::CPU::SSE, tags::CPU::SSE, NONE, JAC, CYCLE::V, MIXED>::value(A, RHS, result, (unsigned long)11, std::numeric_limits<double>::epsilon(), info);
 #else
-                        Multigrid<tags::CPU, tags::CPU, JAC, CYCLE::V, MIXED>::value(A, RHS, result, (unsigned long)11, std::numeric_limits<double>::epsilon(), info);
+                        Multigrid<tags::CPU, tags::CPU, NONE, JAC, CYCLE::V, MIXED>::value(A, RHS, result, (unsigned long)11, std::numeric_limits<double>::epsilon(), info);
 #endif
 
                         //write result to scalarfield:
