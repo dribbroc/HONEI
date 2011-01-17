b1ec84d41b691aaf738d2e6fb77d8a5d5893c394
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2011-01-17 10:22:34 +0100

    Fixed cuda info output formating
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

--- honei/backends/cuda/multi_gpu.cu
+++ honei/backends/cuda/multi_gpu.cu
@@ -47,9 +47,9 @@ extern "C" void cuda_print_device_info(int device)
     printf("Max thread count per block: %i\n", prop.maxThreadsPerBlock);
     printf("Multiprocessor count: %i\n", prop.multiProcessorCount);
     printf("Clock frequenzy: %i kilohertz\n", prop.clockRate);
-    printf("Shared memory per block: %i byte\n", prop.sharedMemPerBlock);
-    printf("Total constant memory: %i byte\n", prop.totalConstMem);
-    printf("Total global memory: %i byte\n", prop.totalGlobalMem);
+    printf("Shared memory per block: %lu byte\n", prop.sharedMemPerBlock);
+    printf("Total constant memory: %lu byte\n", prop.totalConstMem);
+    printf("Total global memory: %lu byte\n", prop.totalGlobalMem);
     printf("Warp size: %i\n", prop.warpSize);
 }
 
