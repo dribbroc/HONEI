94b098f9bcb6361b9ab2f6aabfc067948c7c3dbf
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2011-06-01 14:06:01 +0200

    Added operator infrastructure needed for substituting function pointers in next gen solvers.
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

---
+++ honei/math/operator.hh
@@ -0,0 +1,64 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2011 Markus Geveler <apryde@gmx.de>
+ *
+ * This file is part of the MATH C++ library. LibMath is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibMath is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef MATH_GUARD_OPERATOR_HH
+#define MATH_GUARD_OPERATOR_HH 1
+
+#include <honei/math/defect.hh>
+
+namespace honei
+{
+    class Operator
+    {
+        public:
+            virtual void value() = 0;
+            virtual ~Operator()
+            {
+            }
+    };
+
+    template<typename Tag_, typename MatType_, typename DT_>
+    class DefectOperator : public Operator
+    {
+        public:
+            DefectOperator(DenseVector<DT_>& y, DenseVector<DT_>& b, MatType_& A, DenseVector<DT_>& x) :
+                _y(y),
+                _b(b),
+                _A(A),
+                _x(x)
+            {
+            }
+
+            virtual void value()
+            {
+                Defect<Tag_>::value(_y, _b, _A, _x);
+            }
+
+            virtual ~DefectOperator()
+            {
+            }
+
+        private:
+            DenseVector<DT_> _y;
+            DenseVector<DT_> _b;
+            MatType_ _A;
+            DenseVector<DT_> _x;
+    };
+}
+#endif
---
+++ honei/math/operator_TEST.cc
@@ -0,0 +1,47 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#include <operator.hh>
+#include <honei/util/unittest.hh>
+#include <honei/la/sparse_matrix_ell.hh>
+#include <honei/la/dense_vector.hh>
+#include <honei/math/matrix_io.hh>
+
+using namespace honei;
+using namespace tests;
+using namespace std;
+
+template<typename Tag_>
+class OperatorTest:
+    public BaseTest
+{
+    public:
+        OperatorTest(const std::string & tag) :
+            BaseTest("OperatorTest<" + tag + ">")
+        {
+        }
+
+        virtual void run() const
+        {
+            std::string filename(HONEI_SOURCEDIR);
+            filename += "/honei/math/testdata/poisson_advanced/sort_0/A_7.ell";
+            SparseMatrixELL<double> system(MatrixIO<io_formats::ELL>::read_matrix(filename, double(0)));
+
+            DenseVector<double> result_1(system.rows());
+            DenseVector<double> result_2(system.rows());
+            DenseVector<double> rhs(system.rows(), double(1));
+            DenseVector<double> x(system.rows(), double(0));
+
+            Operator* defop;
+            defop = new DefectOperator<Tag_, SparseMatrixELL<double>, double>(result_1, rhs, system, x);
+
+            defop->value();
+
+            Defect<Tag_>::value(result_2, rhs, system, x);
+
+            for(unsigned long i(0) ; i < x.size() ; ++i)
+                TEST_CHECK_EQUAL_WITHIN_EPS(result_2[i], result_1[i], std::numeric_limits<double>::epsilon());
+
+            delete defop;
+        }
+};
+OperatorTest<tags::CPU> oest_cpu("cpu double");
