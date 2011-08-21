bd04770d85ca2afee7a5b87bc0bc2567d4abd66a
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2011-08-21 18:24:06 +0200

    Added nextgen iterative refinement operation.
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

---
+++ honei/math/ir.hh
@@ -0,0 +1,80 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2011 Markus Geveler <apryde@gmx.de>
+ *
+ * This file is part of the MATH C++ library. LibMath is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibMath is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#pragma once
+#ifndef MATH_GUARD_IR_HH
+#define MATH_GUARD_IR_HH 1
+
+#include<honei/math/mg.hh>
+
+//TODO: plug in solver as type
+//TODO: innertag is only implicitly given in OperatorList cycle
+namespace honei
+{
+    template<typename OuterTag_, typename NormType_>
+    struct IRSolver
+    {
+        public:
+            template<typename OuterMatrixType_,
+                     typename OuterVectorType_,
+                     typename InnerMatrixType_,
+                     typename InnerVectorType_,
+                     typename InnerPreconContType_>
+            static void value(OuterMatrixType_ A, OuterVectorType_ b, OuterVectorType_ x, MGData<InnerMatrixType_, InnerVectorType_, InnerPreconContType_> & data, OperatorList & cycle)
+            {
+                CONTEXT("When solving linear system with Iterative Refinement :");
+                ASSERT(cycle.size() > 0, "OperatorList is empty!");
+                PROFILER_START("IterativeRefinementMGSolver");
+
+                OuterVectorType_ r(x.size());//TODO:move allocation to preprocessing
+                OuterVectorType_ c(x.size());//TODO:move allocation to preprocessing
+                double rnorm_initial(1e16);
+                double rnorm_current(1e16);
+
+                //std::cout << "starting cycles" << std::endl;
+                //inner MG-cycles, TODO: see above
+                for(unsigned long i(0) ; i < data.max_iters ; ++i)
+                {
+                    Defect<OuterTag_>::value(r, b, A, x);
+                    if(i == 0)
+                        rnorm_initial = NormType_::value(r);
+                    else
+                    {
+                        rnorm_current = NormType_::value(r);
+                        data.used_iters = i;
+
+                        if(rnorm_current < data.eps_relative * rnorm_initial)
+                            break;
+                    }
+
+                    convert<OuterTag_>(data.b.at(data.b.size() - 1), r);//TODO: target, then source?
+
+                    cycle.value();//TODO:see above
+
+                    convert<OuterTag_>(c, data.x.at(data.x.size() - 1));//TODO: target, then source?
+
+                    Sum<OuterTag_>::value(x, c);
+
+                }
+
+                PROFILER_STOP("IterativeRefinementMGSolver");
+            }
+    };
+}
+#endif
---
+++ honei/math/ir_TEST.cc
@@ -0,0 +1,106 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#include <honei/math/operator.hh>
+#include <honei/math/restriction.hh>
+#include <honei/math/prolongation.hh>
+#include <honei/math/ir.hh>
+#include <honei/math/methods.hh>
+#include <honei/util/unittest.hh>
+#include <honei/la/sparse_matrix_ell.hh>
+#include <honei/la/dense_vector.hh>
+#include <honei/math/matrix_io.hh>
+#include <honei/math/vector_io.hh>
+
+using namespace honei;
+using namespace tests;
+using namespace std;
+
+template<typename OuterTag_, typename InnerTag_, typename DTOuter_, typename DTInner_>
+class IRSolverTest:
+    public BaseTest
+{
+    public:
+        IRSolverTest(const std::string & tag) :
+            BaseTest("IRSolverTest<" + tag + ">")
+        {
+            register_tag(OuterTag_::name);
+        }
+
+        virtual void run() const
+        {
+            unsigned long levels(4);
+            std::string file(HONEI_SOURCEDIR);
+            file += "/honei/math/testdata/poisson_advanced/sort_0/";
+            MGData<SparseMatrixELL<DTInner_>, DenseVector<DTInner_>, DenseVector<DTInner_> >  data(MGUtil<InnerTag_,
+                                                                                            SparseMatrixELL<DTInner_>,
+                                                                                            DenseVector<DTInner_>,
+                                                                                            DenseVector<DTInner_>,
+                                                                                            io_formats::ELL,
+                                                                                            io_formats::EXP,
+                                                                                            DTInner_>::load_data(file, levels, DTInner_(0.7), "jac"));
+            MGUtil<InnerTag_,
+                SparseMatrixELL<DTInner_>,
+                DenseVector<DTInner_>,
+                DenseVector<DTInner_>,
+                io_formats::ELL,
+                io_formats::EXP,
+                DTInner_>::configure(data, 100, 100, 4, 4, 1, DTInner_(1e-8));
+
+            OperatorList ol(
+                    MGCycleCreation<InnerTag_,
+                    methods::CYCLE::V::STATIC,
+                    CG<InnerTag_, methods::VAR>,
+                    RISmoother<InnerTag_>,
+                    Restriction<InnerTag_, methods::PROLMAT>,
+                    Prolongation<InnerTag_, methods::PROLMAT>,
+                    DTInner_>::value(data)
+                    );
+
+            std::string a_file(HONEI_SOURCEDIR);
+            a_file += "/honei/math/testdata/poisson_advanced/sort_0/A_";
+            a_file += stringify(levels);
+            a_file += ".ell";
+            SparseMatrixELL<DTOuter_> A(MatrixIO<io_formats::ELL>::read_matrix(a_file, DTOuter_(0)));
+
+            std::string b_file(HONEI_SOURCEDIR);
+            b_file += "/honei/math/testdata/poisson_advanced/sort_0/rhs_";
+            b_file += stringify(levels);
+            DenseVector<DTOuter_> b(VectorIO<io_formats::EXP>::read_vector(b_file, DTOuter_(0)));
+
+            std::string x_file(HONEI_SOURCEDIR);
+            x_file += "/honei/math/testdata/poisson_advanced/sort_0/init_";
+            x_file += stringify(levels);
+            DenseVector<DTOuter_> x(VectorIO<io_formats::EXP>::read_vector(x_file, DTOuter_(0)));
+
+            IRSolver<OuterTag_, Norm<vnt_l_two, true, OuterTag_> >::value(A, b, x, data, ol);
+
+            std::cout << data.used_iters << std::endl;
+            std::cout << data.used_iters_coarse << std::endl;
+
+            std::string reffile(HONEI_SOURCEDIR);
+            reffile += "/honei/math/testdata/poisson_advanced/sort_0/sol_";
+            reffile += stringify(levels);
+            DenseVector<double> ref(VectorIO<io_formats::EXP>::read_vector(reffile, double(0)));
+            double base_digits(1);
+            double additional_digits(2);
+
+            double base_eps(1 / pow(10, base_digits));
+            double add_eps(base_eps / pow(10, additional_digits));
+
+            double m((add_eps - base_eps) / double(4));
+            double ba(base_eps - (double(4) * m));
+
+            double eps(m * sizeof(double) + ba);
+            eps *= double(8);
+
+            x.lock(lm_read_only);
+            ref.lock(lm_read_only);
+            for(unsigned long i(0) ; i < ref.size() ; ++i)
+                TEST_CHECK_EQUAL_WITHIN_EPS(x[i], ref[i], eps);
+        }
+};
+#if defined HONEI_CUDA && defined HONEI_SSE
+#ifdef HONEI_CUDA_DOUBLE
+IRSolverTest<tags::CPU::MultiCore::SSE, tags::GPU::CUDA, double, float> ir_solver_test_cpu_gpu("ir mcsse/cuda double/float");
+#endif
+#endif
