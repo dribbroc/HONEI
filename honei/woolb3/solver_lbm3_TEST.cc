1399040f228976ebfef4d7dacd1543ef26d53e55
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2011-09-08 19:30:47 +0200

    Added simple test against old lbm code
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

---
+++ honei/woolb3/solver_lbm3_TEST.cc
@@ -0,0 +1,87 @@
+/* vim: set number sw=4 sts=4 et nofoldenable : */
+
+/*
+ * Copyright (c) 2011 Dirk Ribbrock <dirk.ribbrock@uni-dortmund.de>
+ *
+ * This file is part of the SWE C++ library. HONEI is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * HONEI is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <honei/lbm/solver_lbm_grid.hh>
+#include <honei/lbm/partial_derivative.hh>
+#include <honei/swe/post_processing.hh>
+#include <honei/util/unittest.hh>
+#include <iostream>
+#include <honei/swe/volume.hh>
+#include <honei/math/quadrature.hh>
+#include <honei/lbm/grid.hh>
+#include <honei/lbm/grid_packer.hh>
+#include <honei/lbm/scenario_collection.hh>
+
+#ifdef DEBUG
+#define SOLVER_VERBOSE
+#endif
+
+using namespace honei;
+using namespace tests;
+using namespace std;
+using namespace output_types;
+using namespace lbm::lbm_lattice_types;
+
+//#define SOLVER_VERBOSE
+//#define SOLVER_POSTPROCESSING
+
+template <typename Tag_, typename DataType_>
+class SolverLBM3Test :
+    public TaggedTest<Tag_>
+{
+    public:
+        SolverLBM3Test(const std::string & type) :
+            TaggedTest<Tag_>("solver_lbm3_test<" + type + ">")
+        {
+        }
+
+        virtual void run() const
+        {
+            unsigned long g_h(32);
+            unsigned long g_w(32);
+            unsigned long timesteps(100);
+
+
+            Grid<D2Q9, DataType_> grid;
+
+            ScenarioCollection::get_scenario(0, g_h, g_w, grid);
+
+            PackedGridData<D2Q9, DataType_>  data;
+            PackedGridInfo<D2Q9> info;
+
+            GridPacker<D2Q9, NOSLIP, DataType_>::pack(grid, info, data);
+
+            SolverLBMGrid<Tag_, lbm_applications::LABSWE, DataType_,lbm_force::CENTRED, lbm_source_schemes::BED_FULL, lbm_grid_types::RECTANGULAR, lbm_lattice_types::D2Q9, lbm_boundary_types::NOSLIP, lbm_modes::DRY> solver(&info, &data, grid.d_x, grid.d_y, grid.d_t, grid.tau);
+
+            solver.do_preprocessing();
+            std::cout << "Solving: " << grid.description << std::endl;
+            for (unsigned long i(0) ; i < timesteps ; ++i)
+            {
+                solver.solve();
+            }
+            solver.do_postprocessing();
+            GridPacker<D2Q9, NOSLIP, DataType_>::unpack(grid, info, data);
+            std::cout << *grid.h << std::endl;
+
+            info.destroy();
+            data.destroy();
+            grid.destroy();
+        }
+
+};
+SolverLBM3Test<tags::CPU, float> solver_test_float("float");
