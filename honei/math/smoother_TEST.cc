d3620a044b1b2dcb896317141427a1c3ea8b0971
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2011-10-17 12:25:59 +0200

    Analyse smoothing properties with linear regression
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

--- honei/math/smoother_TEST.cc
+++ honei/math/smoother_TEST.cc
@@ -46,13 +46,13 @@ class SmootherTestSparse:
         {
             //Read in data:
             std::string dir(HONEI_SOURCEDIR);
-            std::string file (dir + "/honei/math/testdata/poisson_advanced2/sort_0/");
-            file += "A_3";
+            std::string file (dir + "/honei/math/testdata/poisson_advanced2/q1t_sort_3/");
+            file += "A_6";
             file += ".ell";
             SparseMatrixELL<DT_> A(MatrixIO<io_formats::ELL>::read_matrix(file, DT_(0)));
 
-            std::string file2 (dir + "/honei/math/testdata/poisson_advanced2/sort_0/");
-            file2 += "rhs_3";
+            std::string file2 (dir + "/honei/math/testdata/poisson_advanced2/q1t_sort_3/");
+            file2 += "rhs_6";
             DenseVector<DT_> rhs(VectorIO<io_formats::EXP>::read_vector(file2, DT_(0)));
 
             /*DenseVector<DT_> diag_inverted(A.rows(), DT_(0));
@@ -60,13 +60,13 @@ class SmootherTestSparse:
             {
                     diag_inverted[i] = DT_(0.7)/A(i, i);
             }*/
-            std::string file5 (dir + "/honei/math/testdata/poisson_advanced2/sort_0/");
-            file5 += "A_3_sainv";
+            std::string file5 (dir + "/honei/math/testdata/poisson_advanced2/q1t_sort_3/");
+            file5 += "A_6_spai_grote";
             file5 += ".ell";
             SparseMatrixELL<DT_> diag_inverted(MatrixIO<io_formats::ELL>::read_matrix(file5, DT_(0)));
 
-            std::string file3 (dir + "/honei/math/testdata/poisson_advanced2/sort_0/");
-            file3 += "init_3";
+            std::string file3 (dir + "/honei/math/testdata/poisson_advanced2/q1t_sort_3/");
+            file3 += "init_6";
             DenseVector<DT_> result(VectorIO<io_formats::EXP>::read_vector(file2, DT_(0)));
 
             DenseVector<DT_> x(rhs.size());
@@ -75,22 +75,19 @@ class SmootherTestSparse:
 
             DenseVector<DT_> temp_0(rhs.size());
             DenseVector<DT_> temp_1(rhs.size());
-            RISmoother<Tag_>::value(A, diag_inverted, rhs, result, temp_0, temp_1, 500ul);
+            RISmoother<Tag_>::value(A, diag_inverted, rhs, result, temp_0, temp_1, 100ul);
 
             Defect<Tag_>::value(x, rhs, A, result);
             DenseVector<DT_> post_y = FFT::forward(x);
-
-            //std::cout<<pre_y<<post_y;
 
             DenseVector<DT_> quotient(post_y.size());
             for (unsigned long i(0) ; i < post_y.size() ; ++i)
             {
                 quotient[i] = (pre_y[i] == DT_(0)) || fabs(post_y[i] / pre_y[i]) > 1 ? DT_(0) : fabs(post_y[i] / pre_y[i]);
             }
-            //std::cout<<quotient;
 
 
-            std::string out_filename (dir + "/honei/math/testdata/");
+            /*std::string out_filename (dir + "/honei/math/testdata/");
             out_filename += "fft.dat";
             std::ofstream hfile;
             hfile.open(out_filename.c_str(), fstream::trunc | fstream::out);
@@ -100,7 +97,50 @@ class SmootherTestSparse:
                 std::string record = stringify(i) + " " + stringify(quotient[i]) + "\n";
                 hfile << record;
             }
+            hfile.close();*/
+
+            // http://de.wikipedia.org/wiki/Kleinste-Quadrate-Sch%C3%A4tzer#Spezialfall_einer_einfachen_linearen_Ausgleichsgeraden
+            DT_ mean_x(0);
+            DT_ mean_y(0);
+            for (unsigned long i(0) ; i < quotient.size() ; ++i)
+            {
+                mean_x += i;
+                mean_y += quotient[i];
+            }
+            mean_x /= quotient.size();
+            mean_y /= quotient.size();
+
+            DenseVector<DT_> dx(quotient.size());
+            DenseVector<DT_> dy(quotient.size());
+            for (unsigned long i(0) ; i < quotient.size() ; ++i)
+            {
+                dx[i] = i - mean_x;
+                dy[i] = quotient[i] - mean_y;
+            }
+
+            DT_ sum_above(0);
+            DT_ sum_beneath(0);
+            for (unsigned long i(0) ; i < quotient.size() ; ++i)
+            {
+                sum_above += dx[i] * dy[i];
+                sum_beneath += dx[i] * dx[i];
+            }
+            DT_ alpha(sum_above / sum_beneath);
+            DT_ alpha_0(mean_y - (alpha * mean_x));
+
+            std::cout<<"alpha: "<<alpha<<" alpha_0: "<<alpha_0<<std::endl;
+
+            std::string out_filename (dir + "/honei/math/testdata/");
+            out_filename += "fft.dat";
+            std::ofstream hfile;
+            hfile.open(out_filename.c_str(), fstream::trunc | fstream::out);
+
+            for (unsigned long i(0) ; i < quotient.size() ; ++i)
+            {
+                std::string record = stringify(i) + " " + stringify(alpha_0 + (DT_(i) * alpha)) + "\n";
+                hfile << record;
+            }
             hfile.close();
         }
 };
-//SmootherTestSparse<tags::CPU, double> smoother_test_double("double");
+SmootherTestSparse<tags::CPU, double> smoother_test_double("double");
--- honei/math/testdata/fft.p
+++ honei/math/testdata/fft.p
@@ -2,11 +2,11 @@ set output "fft.eps"
 set terminal postscript eps enhanced color
 #set size 0.75,0.75
 set notitle
-#set yrange [0:0.1]
+#set yrange [0.24:0.25]
 #set xlabel "x"
 #set ylabel "h + b"
 #set xtics ("0" 0, "1" 100, "2" 199)
 #set line width 2
 plot\
-"fft.dat" title "semi-implicit scheme" with points lt 2 lw 2
+"fft.dat" with points lt 2 lw 2
 
