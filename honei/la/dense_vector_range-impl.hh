f44c53a3c692965c49b2b5dcc3813a7154f7be83
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2012-01-03 10:47:14 +0100

    Do not use private data in other classes
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

--- honei/la/dense_vector_range-impl.hh
+++ honei/la/dense_vector_range-impl.hh
@@ -75,9 +75,9 @@ namespace honei
     {
         CONTEXT("When creating DenseVectorRange:");
         ASSERT(size > 0, "size is zero!");
-        ASSERT(size <= source._imp->size, "size of range is bigger than size of source!");
-        ASSERT(offset <= source._imp->size, "offset is out of bounds!");
-        ASSERT(offset + size <= source._imp->size, "end of range is out of bounds!");
+        ASSERT(size <= source.size(), "size of range is bigger than size of source!");
+        ASSERT(offset <= source.size(), "offset is out of bounds!");
+        ASSERT(offset + size <= source.size(), "end of range is out of bounds!");
     }
 
     template <typename DataType_>
