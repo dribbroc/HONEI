846f2312d34b401d869a0bdc6bc7f75b9cd9845c
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-01-25 11:23:25 +0100

    Applied good configurations to pa benchmarks
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

--- benchmark/mg_pa3_BENCHMARK.cc
+++ benchmark/mg_pa3_BENCHMARK.cc
@@ -118,7 +118,7 @@ class MGBench:
                 PreconContType_,
                 io_formats::ELL,
                 io_formats::EXP,
-                double>::configure(data, 1000, 100, 4, 4, 1, double(1e-8));
+                double>::configure(data, 100, 150, 20, 20, 1, double(1e-8));
 
             OperatorList ol(
                     MGCycleCreation<Tag_,
