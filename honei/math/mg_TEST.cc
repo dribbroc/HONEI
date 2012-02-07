b1ab56b58e9e36c7cb54b860b0bfca6f24087ee6
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2012-02-07 11:50:18 +0100

    Fixed mg_TEST.cc header
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

--- honei/math/mg_TEST.cc
+++ honei/math/mg_TEST.cc
@@ -1,4 +1,23 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2011,2012 Markus Geveler <apryde@gmx.de>
+ * Copyright (c) 2011,2012 Dirk Ribbrock <dirk.ribbrock@uni-dortmund.de>
+ *
+ * This file is part of the HONEI C++ library. LibMath is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibMath is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #ifdef HONEI_GMP
 #include <gmpxx.h>
 #endif
