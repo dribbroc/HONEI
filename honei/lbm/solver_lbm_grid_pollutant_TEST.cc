a325a18f21da5b2a965ecd774d28f8dc33c97f58
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-05-22 15:25:25 +0200

    Added missing test
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

---
+++ honei/lbm/solver_lbm_grid_pollutant_TEST.cc
@@ -0,0 +1,109 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2012 Markus Geveler <apryde@gmx.de>
+ *
+ * This file is part of the SWE C++ library. LiSWE is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * LibSWE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <honei/lbm/solver_lbm_grid.hh>
+#include <honei/lbm/partial_derivative.hh>
+#include <honei/swe/post_processing.hh>
+#include <honei/util/unittest.hh>
+#include <iostream>
+#include <honei/swe/volume.hh>
+#include <honei/math/quadrature.hh>
+#include <honei/lbm/grid.hh>
+#include <honei/lbm/grid_packer.hh>
+#include <honei/lbm/scenario_collection.hh>
+#include <honei/lbm/extraction_grid_pollutant.hh>
+#include <honei/lbm/equilibrium_distribution_grid_pollutant.hh>
+#include <honei/lbm/force_grid_pollutant.hh>
+#include <honei/lbm/collide_stream_grid_pollutant.hh>
+
+#ifdef DEBUG
+#define SOLVER_VERBOSE
+#endif
+
+using namespace honei;
+using namespace tests;
+using namespace std;
+using namespace output_types;
+using namespace lbm::lbm_lattice_types;
+
+//#define SOLVER_VERBOSE
+//#define SOLVER_POSTPROCESSING
+
+template <typename Tag_, typename DataType_>
+class SolverLBMGridPollutantTest :
+    public TaggedTest<Tag_>
+{
+    public:
+        SolverLBMGridPollutantTest(const std::string & type) :
+            TaggedTest<Tag_>("solver_lbm_grid_test<" + type + ">")
+        {
+        }
+
+        virtual void run() const
+        {
+            unsigned long g_h(50);
+            unsigned long g_w(50);
+            unsigned long timesteps(100);
+
+
+            for (unsigned long i(0) ; i < ScenarioCollection::get_stable_scenario_count() ; ++i)
+            {
+                Grid<D2Q9, DataType_> grid;
+
+                ScenarioCollection::get_scenario(i, g_h, g_w, grid);
+
+                PackedGridData<D2Q9, DataType_>  data_flow;
+                PackedGridData<D2Q9, DataType_>  data_poll;
+                PackedGridInfo<D2Q9> info;
+
+                GridPacker<D2Q9, NOSLIP, DataType_>::pack(grid, info, data_flow);
+
+                SolverLBMGrid<Tag_, lbm_applications::LABSWE, DataType_,lbm_force::CENTRED, lbm_source_schemes::BED_FULL, lbm_grid_types::RECTANGULAR, lbm_lattice_types::D2Q9, lbm_boundary_types::NOSLIP, lbm_modes::DRY> solver(&info, &data_flow, grid.d_x, grid.d_y, grid.d_t, grid.tau);
+
+                solver.do_preprocessing();
+                std::cout << "Solving: " << grid.description << std::endl;
+                for(unsigned long i(0); i < timesteps; ++i)
+                {
+#ifdef SOLVER_VERBOSE
+                    std::cout<<"Timestep: " << i << "/" << timesteps << std::endl;
+#endif
+                    solver.solve();
+#ifdef SOLVER_POSTPROCESSING
+                    solver.do_postprocessing();
+                    GridPacker<D2Q9, NOSLIP, DataType_>::unpack(grid, info, data_flow);
+                    PostProcessing<GNUPLOT>::value(*grid.h, 1, g_w, g_h, i);
+#endif
+                }
+                solver.do_postprocessing();
+                GridPacker<D2Q9, NOSLIP, DataType_>::unpack(grid, info, data_flow);
+#ifdef SOLVER_VERBOSE
+                std::cout << *grid.h << std::endl;
+#endif
+                for (unsigned long i(0) ; i < (*grid.h).rows() ; ++i)
+                    for(unsigned long j(0) ; j < (*grid.h).columns() ; ++j)
+                        TEST_CHECK_EQUAL_WITHIN_EPS((*grid.h)( i , j), DataType_(0.02), DataType_(0.1));
+
+                info.destroy();
+                data_flow.destroy();
+                grid.destroy();
+            }
+        }
+
+};
+
+SolverLBMGridPollutantTest<tags::CPU, float> solver_test_float("float");
